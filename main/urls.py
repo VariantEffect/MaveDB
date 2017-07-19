840ef410d51c0f32d38931ac3c43a285104525e1
daniel <danielce90@gmail.com>
2017-07-19 13:36:05 +1000

    new urls and fixed regex issues with accessions

--- main/urls.py
+++ main/urls.py
@@ -7,17 +7,20 @@ from django.conf.urls import url
 
 from .views import home_view, search_view
 from .views import basic_search_view, advanced_search_view
-from .views import experiment_detail_view, dataset_detail_view
+from .views import experiment_detail_view, scoreset_detail_view
 
 from .views import download_view, upload_view, login_register_view
 from .views import usage_guide_view, documentation_view
 from .views import terms_privacy_view, help_contact_view
+
+from .views import new_experiment_view, new_scoreset_view
 
 urlpatterns = [
     url(r'^$', home_view, name='home'),
 
     url(r'download/$', download_view, name='download'),
     url(r'upload/$', upload_view, name='upload'),
+
     url(r'usageguide/$', usage_guide_view, name='usage_guide'),
     url(r'documentation/$', documentation_view, name='documentation'),
     url(r'login/$', login_register_view, name='login'),
@@ -26,9 +29,16 @@ urlpatterns = [
     url(r'help/$', help_contact_view, name='help'),
     url(r'terms/$', terms_privacy_view, name='terms'),
     url(r'privacy/$', terms_privacy_view, name='privacy'),
+
     url(r'search/$', search_view, name='search'),
     url(r'search/basic/$', basic_search_view, name='basic_search'),
     url(r'search/advanced/$', advanced_search_view, name='advanced_search'),
-    url(r'scoreset/(?P<accession>\w+.\d+)/$', dataset_detail_view, name='dataset_detail'),
-    url(r'experiment/(?P<accession>\w+)/$', experiment_detail_view, name='experiment_detail_view')
+
+    url(r'scoreset/(?P<accession>(SCS|scs)\w+.\d+)/$',
+        scoreset_detail_view, name='scoreset_detail'),
+    url(r'scoreset/new/$', new_scoreset_view, name="new_scoreset"),
+
+    url(r'experiment/(?P<accession>(EXP|exp)\d+\w+)/$',
+        experiment_detail_view, name='experiment_detail'),
+    url(r'experiment/new/$', new_experiment_view, name="new_experiment")
 ]
