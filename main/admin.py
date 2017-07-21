85ca28d7b9926fe431915473fe143d9ea80c5105
daniel <danielce90@gmail.com>
2017-07-22 08:54:51 +1000

    Expierment and scoreset admin

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 # from markdownx.admin import MarkdownxModelAdmin
 
-from .models import News, SiteInformation
+from .models import News, SiteInformation, Experiment, ScoreSet
 
 admin.site.register(News)
 admin.site.register(SiteInformation)
+admin.site.register(Experiment)
+admin.site.register(ScoreSet)
 # admin.site.register(SiteInformation, MarkdownxModelAdmin)
