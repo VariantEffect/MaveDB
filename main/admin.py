958774e4c98bfb4fa98115226710a3cf67d3df24
daniel <danielce90@gmail.com>
2017-08-23 10:53:02 +1000

    minor changes to templates

--- main/admin.py
+++ main/admin.py
@@ -7,9 +7,15 @@ admin.site.register(SiteInformation)
 
 from experiment.models import Experiment, ExperimentSet
 from scoreset.models import ScoreSet, Variant
+from .models import Keyword, ExternalAccession, TargetOrganism
+from .models import ReferenceMapping
 
 # Register your models here.
 admin.site.register(ExperimentSet)
 admin.site.register(Experiment)
 admin.site.register(ScoreSet)
 admin.site.register(Variant)
+admin.site.register(Keyword)
+admin.site.register(ExternalAccession)
+admin.site.register(TargetOrganism)
+admin.site.register(ReferenceMapping)
--- main/static/main/main/js/base.js
+++ main/static/main/main/js/base.js
@@ -1,3 +1,4 @@
+// Forward declare jQuery's `$` symbol
 jQuery, $;
 
 // Container variable for common tags on the base tamplate
@@ -47,11 +48,78 @@ $('document').ready(function() {
     });
 });
 
+// Check management form submission
+// ----------------------------------------------------------------------- //
+// `userPk` is a global defined in base.html using Django's templating system.
+
+function askConfirmation() {
+    var saidYes = confirm(
+        'This assignment will remove you as an administartor. If you ' +
+        'continue, you will no longer be able to access this page. ' +
+        'Are you sure?'
+    );
+    return saidYes;
+}
+
+function validate_admin_submit(e) {
+    var aSelected = $("#admin-form > div > select option:selected");
+    var aSelectedPks = aSelected.map(function() {
+        return parseInt(this.value);
+    });
+
+    var willRemoveSelfAsAdmin = aSelectedPks.index(userPk) < 0;
+    if(willRemoveSelfAsAdmin) {
+        var submit = askConfirmation();
+        if(submit) {
+            return $("#admin-form").submit();
+        }
+        return false;
+    }
+    return $("#admin-form").submit();
+  }
+
+function validate_contrib_submit(e) {
+    var cSelected = $("#contrib-form > div > select option:selected");
+    var cSelectedPks = cSelected.map(function() {
+        return parseInt(this.value);
+    });
+
+    var willRemoveSelfAsAdmin = cSelectedPks.index(userPk) >= 0;
+    if(willRemoveSelfAsAdmin) {
+        var submit = askConfirmation();
+        if(submit) {
+            return $("#contrib-form").submit();
+        }
+        return false;
+    }
+    return $("#contrib-form").submit();
+}
+
+function validate_viewer_submit(e) {
+    var vSelected = $("#viewer-form > div > select option:selected");
+    var vSelectedPks = vSelected.map(function() {
+        return parseInt(this.value);
+    });
+
+    var willRemoveSelfAsAdmin = vSelectedPks.index(userPk) >= 0;
+    if(willRemoveSelfAsAdmin) {
+        var submit = askConfirmation();
+        if(submit) {
+            return $("#viewer-form").submit();
+        }
+        return false;
+    }
+    return $("#viewer-form").submit();
+}
+
 // Formsets
 // ----------------------------------------------------------------------- //
 function reset_index(prefix) {
     formset_tag = prefix + '-form-set';
-    form_idx_tag = formset_tag + " > #id_" + prefix.replace('#', '') + "-TOTAL_FORMS";
+    form_idx_tag = formset_tag +
+                    " > #id_" +
+                    prefix.replace('#', '') +
+                    "-TOTAL_FORMS";
 
     var form_idx = parseInt($(form_idx_tag).val());
     element = prefix + "-formset-" + form_idx;
@@ -62,7 +130,10 @@ function reset_index(prefix) {
 
 function add_formset(prefix) {
     formset_tag = prefix + '-form-set';
-    form_idx_tag = formset_tag + " > #id_" + prefix.replace('#', '') + "-TOTAL_FORMS";
+    form_idx_tag = formset_tag +
+                    " > #id_" +
+                    prefix.replace('#', '') +
+                    "-TOTAL_FORMS";
     template_form_tag = prefix + "-empty-form";
     empty_set_tag = prefix + '-empty-set';
 
@@ -86,14 +157,17 @@ function add_formset(prefix) {
 
 function remove_formset(prefix) {
     formset_tag = prefix + '-form-set';
-    form_idx_tag = formset_tag + " > #id_" + prefix.replace('#', '') + "-TOTAL_FORMS";
+    form_idx_tag = formset_tag +
+                    " > #id_" +
+                    prefix.replace('#', '') +
+                    "-TOTAL_FORMS";
     template_form_tag = prefix + "-empty-form";
     empty_set_tag = prefix + '-empty-set';
 
     var form_idx = parseInt($(form_idx_tag).val());
     element = prefix + "-formset-" + form_idx;
 
-    possible_elemet = prefix + "-formset-" + 1; 
+    possible_elemet = prefix + "-formset-" + 1;
     if (form_idx === 0 && $(possible_elemet).length > 0) {
         $(possible_elemet).remove();
         $(empty_set_tag).show();
