8c8bd6c16ac48f3621e2ad5c0eb4945249d04c70
daniel <danielce90@gmail.com>
2017-08-24 18:32:31 +1000

    updated views to reflect form changes

--- scoreset/views.py
+++ scoreset/views.py
@@ -35,12 +35,12 @@ KEYWORD_FORM_PREFIX = "keyword"
 KeywordFormSet = formset_factory(KeywordForm)
 
 
-class ScoresetDetailView(DetailView):
+class ScoreSetDetailView(DetailView):
     """
     Simple detail view. See `scoreset/scoreset.html` for the template
     layout.
     """
-    model = Experiment
+    model = ScoreSet
     template_name = 'scoreset/scoreset.html'
     context_object_name = "scoreset"
 
@@ -66,7 +66,7 @@ class ScoresetDetailView(DetailView):
             response.status_code = 403
             return response
         else:
-            return super(ScoresetDetailView, self).dispatch(
+            return super(ScoreSetDetailView, self).dispatch(
                 request, *args, **kwargs
             )
 
@@ -75,8 +75,9 @@ class ScoresetDetailView(DetailView):
         return get_object_or_404(ScoreSet, accession=accession)
 
     def get_context_data(self, **kwargs):
-        context = super(ScoresetDetailView, self).get_context_data(**kwargs)
-        variant_list = Variant.objects.all()
+        context = super(ScoreSetDetailView, self).get_context_data(**kwargs)
+        instance = self.get_object()
+        variant_list = instance.variant_set.all()
         paginator = Paginator(variant_list, 100)
 
         try:
@@ -176,43 +177,6 @@ def download_scoreset_metadata(request, accession):
     return HttpResponse(json_response, content_type="application/json")
 
 
-def parse_text_formset(formset, model_class, prefix):
-    """
-    Helper function to parse a keyword formset into individual `Keyword`
-    objects that can be saved later. If a keyword is not valid, it is appended
-    as `None`.
-
-    Parameters
-    ----------
-    formset : `KeywordFormSet`
-        The formset to parse.
-    model_class : `Keyword`
-        The class that the formset instantiates.
-    prefix : `str`
-        The prefix of the formset. Should be set as 'keyword'.
-
-    Returns
-    -------
-    `list`
-        A list of valid but unsaved `Keyword`s
-    """
-    objects = []
-    for i, form in enumerate(formset):
-        text = form.data.get("{}-{}-text".format(prefix, i), "")
-        try:
-            model = model_class.objects.get(text=text)
-            objects.append(model)
-        except ObjectDoesNotExist:
-            if form.is_valid():
-                if form.cleaned_data:
-                    model = form.save(commit=False)
-                    if model.text not in set([o.text for o in objects]):
-                        objects.append(model)
-            else:
-                objects.append(None)
-    return objects
-
-
 @login_required(login_url=reverse_lazy("accounts:login"))
 def scoreset_create_view(request):
     """
@@ -225,41 +189,21 @@ def scoreset_create_view(request):
     """
     context = {}
     scoreset_form = ScoreSetForm(prefix=SCORESET_FORM_PREFIX)
-    keyword_formset = KeywordFormSet(prefix=KEYWORD_FORM_PREFIX)
     context["scoreset_form"] = scoreset_form
-    context["keyword_formset"] = keyword_formset
 
     if request.method == "POST":
         scoreset_form = ScoreSetForm(
             request.POST, prefix=SCORESET_FORM_PREFIX)
-        keyword_formset = KeywordFormSet(
-            request.POST, prefix=KEYWORD_FORM_PREFIX)
         context["scoreset_form"] = scoreset_form
-        context["keyword_formset"] = keyword_formset
 
         if scoreset_form.is_valid():
-            scoreset = scoreset_form.save(commit=False)
+            scoreset = scoreset_form.save(commit=True)
         else:
             return render(
                 request,
                 "scoreset/new_scoreset.html",
                 context=context
             )
-
-        keywords = parse_text_formset(keyword_formset, Keyword, "keyword")
-        if not all([k is not None for k in keywords]):
-            return render(
-                request,
-                "scoreset/new_scoreset.html",
-                context=context
-            )
-
-        # Looks like everything is good to save.
-        scoreset.save()
-        scoreset_form.save_variants()
-        for kw in keywords:
-            kw.save()
-            scoreset.keywords.add(kw)
 
         # Save and update permissions. A user will not be added as an
         # admin to the parent experiment. This must be done by the admin
