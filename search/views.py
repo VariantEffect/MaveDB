45fce0794a504933f71f3f6991caa05f733680d5
daniaki <danielce90@gmail.com>
2017-09-04 16:13:28 +1000

    search table displays public items unless logged in to see own private datasets

--- search/views.py
+++ search/views.py
@@ -3,23 +3,35 @@ from django.shortcuts import render
 from experiment.models import Experiment
 from scoreset.models import ScoreSet
 
+from accounts.permissions import user_is_anonymous
+
 from .forms import SearchForm
 
 
 def search_view(request):
     form = SearchForm()
     experiments = Experiment.objects.all()
-    scoresets = ScoreSet.objects.all()
+
     if request.method == "GET":
         form = SearchForm(request.GET)
         if form.is_valid():
             experiments = form.query_experiments()
-            scoresets = form.query_scoresets()
-            print(scoresets)
+
+    if user_is_anonymous(request.user):
+        experiments = [e for e in experiments if not e.private]
+        has_data = len(experiments) > 0
+    else:
+        users_experiments = request.user.profile.experiments()
+        private_experiments = [
+            e for e in experiments if e.private and e in users_experiments
+        ]
+        public_experiments = [e for e in experiments if not e.private]
+        has_data = len(private_experiments) > 0 or len(public_experiments) > 0
 
     context = {
         "form": form,
-        'experiments': experiments,
-        'scoresets': scoresets
+        "experiments": experiments,
+        "has_data": has_data
     }
+
     return render(request, "search/search.html", context)
