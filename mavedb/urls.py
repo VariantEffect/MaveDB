3608916485be0ef7224e32a0170781e87c6b43f3
daniaki <danielce90@gmail.com>
2017-09-18 16:32:13 +1000

    added api to urls

--- mavedb/urls.py
+++ mavedb/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('^', include('social_django.urls', namespace='social')),
     url(r'^', include('main.urls', namespace='main'), name='main'),
+    url(r'^', include('api.urls', namespace='api'), name='api'),
     url(
         r'^accounts/',
         include('accounts.urls', namespace='accounts'), name='accounts'
