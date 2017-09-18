34b285ef1417e5c4b7e12a4d8d15ef78ce851e01
daniaki <danielce90@gmail.com>
2017-09-18 18:33:13 +1000

    removed old authentication steps

--- accounts/urls.py
+++ accounts/urls.py
@@ -23,7 +23,6 @@ from django.urls import reverse_lazy
 import django.contrib.auth.views as auth_views
 
 from .views import registration_view, profile_view
-from .views import activate_account_view, send_activation_email_view
 from .views import manage_instance, edit_instance, view_instance
 from .views import login_with_remember_me, list_all_users_and_their_data
 
@@ -34,10 +33,6 @@ urlpatterns = [
 
     # ------ Register
     url(r"register/$", registration_view, name="register"),
-    url(r"activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
-        activate_account_view, name="activate"),
-    url(r"activate/(?P<uidb64>[0-9A-Za-z_\-]+)/$",
-        send_activation_email_view, name='send_activation_email'),
 
     # ------ Login and Logout
     url(
@@ -48,26 +43,6 @@ urlpatterns = [
         r'logout/$',
         auth_views.LogoutView.as_view(),
         name='logout'),
-
-    # ------ Password Resetting
-    url(
-        r'password_reset/$',
-        auth_views.PasswordResetView.as_view(
-            success_url=reverse_lazy("accounts:password_reset_done")),
-        name='password_reset'),
-    url(
-        r'password_reset/done/$',
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done"),
-    url(
-        r'reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.PasswordResetConfirmView.as_view(
-            success_url=reverse_lazy("accounts:password_reset_complete")),
-        name="password_reset_confirm"),
-    url(
-        r'reset/done/$',
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete"),
 
     # ------ Profile
     url(r"^$", profile_view, name="index"),
--- accounts/views.py
+++ accounts/views.py
@@ -444,81 +444,15 @@ def view_instance(request, accession):
 
 # Registration views
 # ------------------------------------------------------------------------- #
-def activate_account_view(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        render(request, 'accounts/account_not_created.html')
-
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        return redirect('accounts:profile')
-    else:
-        context = {'uidb64': uidb64}
-        return render(request, 'accounts/activation_invalid.html', context)
-
-
-def send_activation_email_view(request, uidb64):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is None:
-        return render(request, 'accounts/account_not_created.html')
-
-    # We have the User object, now try to send an email. If the new uidb64
-    # or token could not be made, abort the send/resend cycle.
-    uidb64, token = send_user_activation_email(
-        uid=user.pk,
-        secure=request.is_secure(),
-        domain=get_current_site(request).domain,
-        subject='Activate Your Account',
-        template_name='accounts/activation_email.html')
-
-    if uidb64 is None or token is None:
-        return render(request, 'accounts/account_not_created.html')
-    else:
-        context = {'uidb64': uidb64}
-        return render(request, 'accounts/activation_sent.html', context)
-
-
 def registration_view(request):
     form = RegistrationForm()
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            # Additional hacked-on checking to see if email is unique.
-            email = form.cleaned_data['email']
-
-            if User.objects.filter(email__iexact=email).count() > 0:
-                form.add_error(
-                    "email", ValidationError("This email is already in use."))
-            else:
-                user = form.save(commit=False)
-                user.is_active = True
-                user.save()
-                uidb64, token = send_user_activation_email(
-                    uid=user.pk,
-                    secure=request.is_secure(),
-                    domain=get_current_site(request).domain,
-                    subject='Activate Your Account',
-                    template_name='accounts/activation_email.html')
-
-                if uidb64 is None or token is None:
-                    return render(
-                        request,
-                        'accounts/account_not_created.html'
-                    )
-                else:
-                    context = {'uidb64': uidb64}
-                    return render(
-                        request,
-                        'accounts/activation_sent.html',
-                        context
-                    )
+            user = form.save(commit=False)
+            user.is_active = True
+            user.save()
+            return redirect('accounts:profile')
 
     context = {'form': form}
-    return render(request, 'accounts/register.html', context)
+    return render(request, 'registration/register.html', context)
