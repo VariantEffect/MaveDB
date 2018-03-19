7d74c12b94b62844dd16126492fb698de9e0f727
daniel <danielce90@gmail.com>
2018-03-19 16:41:23 +1100

    fixed broken imports

--- main/admin.py
+++ main/admin.py
@@ -5,11 +5,10 @@ from django.db.models import ObjectDoesNotExist
 from reversion.admin import VersionAdmin
 
 from experiment.models import Experiment, ExperimentSet
-from scoreset.models import ScoreSet, Variant
+from experiment.models import Keyword, ExternalAccession, TargetOrganism
+from experiment.models import ScoreSet, Variant
 
 from .models import News, SiteInformation, Licence
-from .models import Keyword, ExternalAccession, TargetOrganism
-from .models import ReferenceMapping
 
 
 # Register with admin
@@ -20,7 +19,6 @@ admin.site.register(SiteInformation)
 admin.site.register(TargetOrganism)
 admin.site.register(Keyword)
 admin.site.register(ExternalAccession)
-admin.site.register(ReferenceMapping)
 
 
 admin.site.register(Variant)
