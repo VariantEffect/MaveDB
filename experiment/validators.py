743cd6beae8cbba271d50a88568e69c1017db375
daniel <danielce90@gmail.com>
2018-03-19 16:45:01 +1100

    shifted to experiment app

--- experiment/validators.py
+++ experiment/validators.py
@@ -1,6 +1,7 @@
 
 import re
 from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
 
 DNA_SEQ_RE = r'[ATGCatgc]+'
 
@@ -31,6 +32,10 @@ MAVEDB_ANY_URN_PATTERN = '|'.join([r'({pattern})'.format(pattern=p) for p in (
     MAVEDB_SCORESET_URN_PATTERN,
     MAVEDB_VARIANT_URN_PATTERN)
 ])
+
+csv_validator = FileExtensionValidator(
+    allowed_extensions=['csv']
+)
 
 def valid_mavedb_urn(accession):
     if not re.match(MAVEDB_ANY_URN_PATTERN, accession):
@@ -78,3 +83,211 @@ def valid_wildtype_sequence(seq):
             "'%(seq)s' is not a valid wild type sequence.",
             params={"seq": seq}
         )
+
+
+# --------------------------------------------------------------------------- #
+#                           ScoreSet Validators
+# --------------------------------------------------------------------------- #
+#: Matches a single amino acid substitution in HGVS_ format.
+re_protein = re.compile(
+    "(?P<match>p\.(?P<pre>[A-Z][a-z][a-z])(?P<pos>-?\d+)"
+    "(?P<post>[A-Z][a-z][a-z]))")
+
+
+#: Matches a single nucleotide substitution (coding or noncoding)
+#: in HGVS_ format.
+re_nucleotide = re.compile(
+    "(?P<match>[nc]\.(?P<pos>-?\d+)(?P<pre>[ACGT])>(?P<post>[ACGT]))")
+
+
+#: Matches a single coding nucleotide substitution in HGVS_ format.
+re_coding = re.compile(
+    "(?P<match>c\.(?P<pos>-?\d+)(?P<pre>[ACGT])>(?P<post>[ACGT]) "
+    "\(p\.(?:=|[A-Z][a-z][a-z]-?\d+[A-Z][a-z][a-z])\))")
+
+
+#: Matches a single noncoding nucleotide substitution in HGVS_ format.
+re_noncoding = re.compile(
+    "(?P<match>n\.(?P<pos>-?\d+)(?P<pre>[ACGT])>(?P<post>[ACGT]))")
+
+
+
+class Constants(object):
+    NAN_COL_VALUES = set(['nan', 'na', 'none', '', ' '])
+    HGVS_COLUMN = "hgvs"
+    COUNT_COLUMN = "count"
+    SCORES_DATA = "scores_data"
+    COUNTS_DATA = "counts_data"
+    SCORES_KEY = 'score'
+    COUNTS_KEY = 'count'
+
+
+def is_null(value):
+    return str(value).strip().lower() in Constants.NAN_COL_VALUES
+
+
+def valid_hgvs_string(string):
+   # variants = [v.strip() for v in string.strip().split(',')]
+   #
+   # protein_matches = all([re_protein.match(v) for v in variants])
+   # nucleotide_matches = all([re_nucleotide.match(v) for v in variants])
+   # coding_matches = all([re_coding.match(v) for v in variants])
+   # noncoding_matches = all([re_noncoding.match(v) for v in variants])
+   # wt_or_sy = all([v in ["_wt", "_sy"] for v in variants])
+   #
+   # if not (protein_matches or nucleotide_matches or
+   #         coding_matches or noncoding_matches or wt_or_sy):
+   #     raise ValidationError(
+   #         _("Variant '%(variant)s' is not a valid HGVS string."),
+   #         params={'variant': string}
+   #     )
+    return
+
+
+def valid_scoreset_score_data_input(file):
+    header_line = file.readline()
+    if isinstance(header_line, bytes):
+        header_line = header_line.decode()
+
+    expected_header = [Constants.HGVS_COLUMN]
+    header = [l.strip() for l in header_line.strip().split(',')]
+    try:
+        valid_header = header[0] == expected_header[0]
+        if not valid_header:
+            raise ValueError()
+    except:
+        raise ValidationError(
+            _(
+                "Score data header requires the column 'hgvs'. Ensure the "
+                "columns are separated by a comma, and that 'hgvs' is the "
+                "first column."
+            )
+        )
+    if file.size < 4:
+        raise ValidationError(_("Dataset cannot be empty."))
+
+
+def valid_scoreset_count_data_input(file):
+    header_line = file.readline()
+    if isinstance(header_line, bytes):
+        header_line = header_line.decode()
+
+    expected_header = [Constants.HGVS_COLUMN]
+    header = [l.strip() for l in header_line.strip().split(',')]
+    try:
+        valid_header = (header[0] == expected_header[0])
+        if not valid_header:
+            raise ValueError()
+    except:
+        raise ValidationError(
+            _(
+                "Count data header requires the column 'hgvs'. Ensure the "
+                "columns are separated by a comma, and that 'hgvs' is the "
+                "first column."
+            )
+        )
+    if file.size < 4:
+        raise ValidationError(_("Dataset cannot be empty."))
+
+
+def valid_scoreset_json(json_object, has_counts_data=False):
+    if Constants.SCORES_KEY not in json_object.keys():
+        raise ValidationError(
+            _("%(data)s is missing the required key '%(key)s'."),
+            params={"data": json_object, "key": Constants.SCORES_KEY}
+        )
+    if Constants.COUNTS_KEY not in json_object.keys():
+        raise ValidationError(
+            _("%(data)s is missing the required key '%(key)s'."),
+            params={"data": json_object, "key": Constants.COUNTS_KEY}
+        )
+    expected = [Constants.SCORES_KEY, Constants.COUNTS_KEY]
+    extras = [k for k in json_object.keys() if k not in set(expected)]
+    if len(extras) > 0:
+        extras = [k for k in json_object.keys() if k not in expected]
+        raise ValidationError(
+            _("%(data)s has additional unexpected keys '%(extras)s'."),
+            params={"data": json_object, "extras": extras}
+        )
+
+    # Check the correct data types are given.
+    if not isinstance(json_object[Constants.SCORES_KEY], list):
+        type_ = type(json_object[Constants.SCORES_KEY]).__name__
+        raise ValidationError(
+            _("%(data)s dictionary values must be a list not %(type)s."),
+            params={"data": json_object, "type": type_}
+        )
+    if not isinstance(json_object[Constants.COUNTS_KEY], list):
+        type_ = type(json_object[Constants.COUNTS_KEY]).__name__
+        raise ValidationError(
+            _("%(data)s dictionary values must be a list not %(type)s."),
+            params={"data": json_object, "type": type_}
+        )
+
+    # Check the correct inner data types are given.
+    try:
+        if not isinstance(json_object[Constants.SCORES_KEY][0], str):
+            raise ValidationError("")
+    except IndexError:
+        raise ValidationError(
+            _("No header could be found for '%(key)s' dataset."),
+            params={"key": Constants.SCORES_KEY}
+        )
+    except ValidationError:
+        type_ = type(json_object[Constants.SCORES_KEY][0]).__name__
+        raise ValidationError(
+            _("Header values must be strings not '%(type)s'."),
+            params={"type": type_}
+        )
+
+    try:
+        if not isinstance(json_object[Constants.COUNTS_KEY][0], str):
+            raise ValidationError()
+    except IndexError:
+        if has_counts_data:
+            raise ValidationError(
+                _("No header could be found for '%(key)s' dataset."),
+                params={"key": Constants.COUNTS_KEY}
+            )
+    except ValidationError:
+        type_ = type(json_object[Constants.COUNTS_KEY][0]).__name__
+        raise ValidationError(
+            _("Header values must be strings not '%(type)s'."),
+            params={"type": type_}
+        )
+
+
+def valid_variant_json(json_object):
+    if Constants.SCORES_KEY not in json_object.keys():
+        raise ValidationError(
+            _("%(data)s is missing the required key '%(key)s'."),
+            params={"data": json_object, "key": Constants.SCORES_KEY}
+        )
+    if Constants.COUNTS_KEY not in json_object.keys():
+        raise ValidationError(
+            _("%(data)s is missing the required key '%(key)s'."),
+            params={"data": json_object, "key": Constants.COUNTS_KEY}
+        )
+    expected = [Constants.SCORES_KEY, Constants.COUNTS_KEY]
+    extras = [k for k in json_object.keys() if k not in set(expected)]
+    if len(extras) > 0:
+        extras = [k for k in json_object.keys() if k not in expected]
+        raise ValidationError(
+            _("%(data)s has additional unexpected keys '%(extras)s'."),
+            params={"data": json_object, "extras": extras}
+        )
+
+    # Check the correct data types are given.
+    if not isinstance(json_object[Constants.SCORES_KEY], dict):
+        type_ = type(json_object[Constants.SCORES_KEY]).__name__
+        raise ValidationError(
+            _("%(data)s dictionary values must be a dict not %(type)s."),
+            params={"data": json_object, "type": type_}
+        )
+    if not isinstance(json_object[Constants.COUNTS_KEY], dict):
+        type_ = type(json_object[Constants.COUNTS_KEY]).__name__
+        raise ValidationError(
+            _("%(data)s dictionary values must be a dict not %(type)s."),
+            params={"data": json_object, "type": type_}
+        )
+
--- experiment/views.py
+++ experiment/views.py
@@ -1,34 +1,37 @@
+import json
+
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseForbidden, Http404
+from django.http import HttpResponse, JsonResponse, StreamingHttpResponse
+from django.http import HttpResponseForbidden, Http404
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import DetailView
-from django.forms import formset_factory
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.forms import formset_factory
 
-from accounts.permissions import (
-    assign_user_as_instance_admin, PermissionTypes
-)
 
+from main.utils.pandoc import convert_md_to_html
 from main.utils.versioning import save_and_create_revision_if_tracked_changed
-from main.fields import ModelSelectMultipleField as msmf
-from main.forms import (
-    KeywordForm, ExternalAccessionForm,
-    ReferenceMappingForm, TargetOrganismForm
-)
-from main.models import (
-    Keyword, ExternalAccession,
-    TargetOrganism, ReferenceMapping
-)
 
-from scoreset.views import scoreset_create_view
+from accounts.forms import send_admin_email
+from accounts.permissions import assign_user_as_instance_admin, PermissionTypes
 
-from .models import Experiment, ExperimentSet
-from .forms import ExperimentForm
+from .validators import Constants
+from .fields import ModelSelectMultipleField as msmf
+from .models import Experiment, ExperimentSet, ScoreSet
+from .forms import ExperimentForm, ExperimentEditForm
+from .forms import ScoreSetForm, ScoreSetEditForm
+from .forms import KeywordForm, TargetOrganismForm, ExternalAccessionForm
 
 
-ReferenceMappingFormSet = formset_factory(ReferenceMappingForm)
-REFERENCE_MAPPING_FORM_PREFIX = "reference_mapping"
+
+SCORESET_FORM_PREFIX = "scoreset"
+KEYWORD_FORM_PREFIX = "keyword"
+KeywordFormSet = formset_factory(KeywordForm)
+
+
+
 
 
 class ExperimentDetailView(DetailView):
@@ -176,16 +179,11 @@ def experiment_create_view(request):
 
     experiment_form = ExperimentForm()
     experiment_form.fields["experimentset"].queryset = experimentsets
-    ref_mapping_formset = ReferenceMappingFormSet(
-        prefix=REFERENCE_MAPPING_FORM_PREFIX
-    )
     context["experiment_form"] = experiment_form
-    context["reference_mapping_formset"] = ref_mapping_formset
 
     # If you change the prefix arguments here, make sure to change them
     # in base.js as well for the +/- buttons to work correctly.
     if request.method == "POST":
-
         # Get the new keywords/accession/target org so that we can return
         # them for list repopulation if the form has errors.
         keywords = request.POST.getlist("keywords")
@@ -197,28 +195,18 @@ def experiment_create_view(request):
 
         experiment_form = ExperimentForm(request.POST)
         experiment_form.fields["experimentset"].queryset = experimentsets
-        ref_mapping_formset = ReferenceMappingFormSet(
-            request.POST, prefix=REFERENCE_MAPPING_FORM_PREFIX
-        )
         context["experiment_form"] = experiment_form
-        context["reference_mapping_formset"] = ref_mapping_formset
         context["repop_keywords"] = ','.join(keywords)
         context["repop_external_accessions"] = ','.join(e_accessions)
         context["repop_target_organism"] = ','.join(target_organism)
 
-        maps = parse_mapping_formset(ref_mapping_formset)
-        if not all([m is not None for m in maps]) or \
-                not experiment_form.is_valid():
+        if not experiment_form.is_valid():
             return render(
                 request,
                 "experiment/new_experiment.html",
                 context=context
             )
-
         experiment = experiment_form.save(commit=True)
-        for ref_map in maps:
-            ref_map.experiment = experiment
-            ref_map.save()
 
         # Save and update permissions. If no experimentset was selected,
         # by default a new experimentset is created with the current user
@@ -246,5 +234,270 @@ def experiment_create_view(request):
         return render(
             request,
             "experiment/new_experiment.html",
+            context=context
+        )
+
+
+
+# --------------------------------------------------------------------------- #
+#                               ScoreSet Views
+# --------------------------------------------------------------------------- #
+class ScoreSetDetailView(DetailView):
+    """
+    Simple detail view. See `scoreset/scoreset.html` for the template
+    layout.
+    """
+    model = ScoreSet
+    template_name = 'scoreset/scoreset.html'
+    context_object_name = "scoreset"
+
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            scoreset = self.get_object()
+        except Http404:
+            response = render(
+                request=request,
+                template_name="main/404_not_found.html"
+            )
+            response.status_code = 404
+            return response
+
+        has_permission = self.request.user.has_perm(
+            PermissionTypes.CAN_VIEW, scoreset)
+        if scoreset.private and not has_permission:
+            response = render(
+                request=request,
+                template_name="main/403_forbidden.html",
+                context={"instance": scoreset},
+            )
+            response.status_code = 403
+            return response
+        else:
+            return super(ScoreSetDetailView, self).dispatch(
+                request, *args, **kwargs
+            )
+
+    def get_object(self):
+        accession = self.kwargs.get('accession', None)
+        return get_object_or_404(ScoreSet, accession=accession)
+
+    def get_context_data(self, **kwargs):
+        context = super(ScoreSetDetailView, self).get_context_data(**kwargs)
+        instance = self.get_object()
+        scores_variant_list = instance.variant_set.all().order_by("hgvs")
+
+        if instance.has_counts_dataset():
+            counts_variant_list = instance.variant_set.all().order_by("hgvs")
+        else:
+            counts_variant_list = instance.variant_set.none()
+
+        try:
+            counts_per_page = self.request.GET.get('counts-per-page', '')
+            counts_per_page = int(counts_per_page)
+        except:
+            counts_per_page = 20
+
+        try:
+            scores_per_page = self.request.GET.get('scores-per-page', '')
+            scores_per_page = int(scores_per_page)
+        except:
+            scores_per_page = 20
+
+        scores_paginator = Paginator(
+            scores_variant_list, per_page=scores_per_page)
+        counts_paginator = Paginator(
+            counts_variant_list, per_page=counts_per_page)
+
+        try:
+            scores_page = self.request.GET.get('scores-page', None)
+            scores_variants = scores_paginator.page(scores_page)
+        except PageNotAnInteger:
+            scores_page = 1
+            scores_variants = scores_paginator.page(scores_page)
+        except EmptyPage:
+            scores_page = scores_paginator.num_pages
+            scores_variants = scores_paginator.page(scores_page)
+
+        try:
+            counts_page = self.request.GET.get('counts-page', None)
+            counts_variants = counts_paginator.page(counts_page)
+        except PageNotAnInteger:
+            counts_page = 1
+            counts_variants = counts_paginator.page(counts_page)
+        except EmptyPage:
+            counts_page = counts_paginator.num_pages
+            counts_variants = counts_paginator.page(counts_page)
+
+        # Handle the case when there are too many pages for scores.
+        index = scores_paginator.page_range.index(scores_variants.number)
+        max_index = len(scores_paginator.page_range)
+        start_index = index - 5 if index >= 5 else 0
+        end_index = index + 5 if index <= max_index - 5 else max_index
+        page_range = scores_paginator.page_range[start_index:end_index]
+        context["scores_page_range"] = page_range
+        context["scores_variants"] = scores_variants
+        context["scores_columns"] = \
+            context['scoreset'].dataset_columns[Constants.SCORES_KEY]
+
+        # Handle the case when there are too many pages for counts.
+        index = counts_paginator.page_range.index(counts_variants.number)
+        max_index = len(counts_paginator.page_range)
+        start_index = index - 5 if index >= 5 else 0
+        end_index = index + 5 if index <= max_index - 5 else max_index
+        page_range = counts_paginator.page_range[start_index:end_index]
+        context["counts_page_range"] = page_range
+        context["counts_variants"] = counts_variants
+        context["counts_columns"] = \
+            context['scoreset'].dataset_columns[Constants.COUNTS_KEY]
+
+        context["scores_per_page"] = scores_per_page
+        context["counts_per_page"] = counts_per_page
+        context["per_page_selections"] = [20, 50, 100]
+
+        return context
+
+
+def download_scoreset_data(request, accession, dataset_key):
+    """
+    This view returns the variant dataset in csv format for a specific
+    `ScoreSet`. This will either be the 'scores' or 'counts' dataset, which
+    are the only two supported keys in a scoreset's `dataset_columns`
+    attributes.
+
+    Parameters
+    ----------
+    accession : `str`
+        The `ScoreSet` accession which will be queried.
+    dataset_key : `str`
+        The type of dataset requested. Currently this is either 'scores' or
+        'counts' as these are the only two supported datasets.
+
+    Returns
+    -------
+    `StreamingHttpResponse`
+        A stream is returned to handle the case where the data is too large
+        to send all at once.
+    """
+    scoreset = get_object_or_404(ScoreSet, accession=accession)
+
+    has_permission = request.user.has_perm(
+        PermissionTypes.CAN_VIEW, scoreset
+    )
+    if scoreset.private and not has_permission:
+        response = render(
+            request=request,
+            template_name="main/403_forbidden.html",
+            context={"instance": scoreset},
+        )
+        response.status_code = 403
+        return response
+
+    if not scoreset.has_counts_dataset() and dataset_key == Constants.COUNTS_KEY:
+        return StreamingHttpResponse("", content_type='text')
+
+    variants = scoreset.variant_set.all().order_by("accession")
+    columns = scoreset.dataset_columns[dataset_key]
+
+    def gen_repsonse():
+        yield ','.join(columns) + '\n'
+        for var in variants:
+            data = []
+            for column_key in columns:
+                data.append(str(var.data[dataset_key][column_key]))
+            yield ','.join(data) + '\n'
+
+    return StreamingHttpResponse(gen_repsonse(), content_type='text')
+
+
+@login_required(login_url=reverse_lazy("accounts:login"))
+def scoreset_create_view(request, came_from_new_experiment=False, e_accession=None):
+    """
+    A view to create a new scoreset. Upon successs, this view will redirect
+    to the newly created scoreset object.
+
+    If you change the prefix arguments here, make sure to change them
+    in the corresponding template element id fields as well. If you don't,
+    expect everything to break horribly.
+    """
+    context = {}
+    scoreset_form = ScoreSetForm()
+
+    pks = [i.pk for i in request.user.profile.administrator_experiments()]
+    experiments = Experiment.objects.filter(
+        pk__in=set(pks)
+    ).order_by("accession")
+    scoreset_form.fields["experiment"].queryset = experiments
+
+    pks = [i.pk for i in request.user.profile.administrator_scoresets()]
+    scoresets = ScoreSet.objects.filter(pk__in=set(pks)).order_by("accession")
+    scoreset_form.fields["replaces"].queryset = scoresets
+
+    if came_from_new_experiment:
+        experiments = Experiment.objects.filter(accession=e_accession)
+        scoreset_form.fields["experiment"].queryset = experiments
+        context["scoreset_form"] = scoreset_form
+        context["came_from_new_experiment"] = came_from_new_experiment
+        context["e_accession"] = e_accession
+        return render(
+            request,
+            "scoreset/new_scoreset.html",
+            context=context
+        )
+
+    # If the request is ajax, then it's for previewing the abstract
+    # or method description
+    if request.is_ajax():
+        data = {}
+        data['abstract'] = convert_md_to_html(
+            request.GET.get("abstract", "")
+        )
+        data['method_desc'] = convert_md_to_html(
+            request.GET.get("method_desc", "")
+        )
+        return HttpResponse(json.dumps(data), content_type="application/json")
+
+    if request.method == "POST":
+        # Get the new keywords so that we can return them for
+        # list repopulation if the form has errors.
+        keywords = request.POST.getlist("keywords")
+        keywords = [kw for kw in keywords if msmf.is_word(kw)]
+
+        scoreset_form = ScoreSetForm(data=request.POST, files=request.FILES)
+        scoreset_form.fields["experiment"].queryset = experiments
+        scoreset_form.fields["replaces"].queryset = scoresets
+        context["repop_keywords"] = ','.join(keywords)
+        context["scoreset_form"] = scoreset_form
+
+        if scoreset_form.is_valid():
+            scoreset = scoreset_form.save(commit=True)
+        else:
+            return render(
+                request,
+                "scoreset/new_scoreset.html",
+                context=context
+            )
+
+        # Save and update permissions. A user will not be added as an
+        # admin to the parent experiment. This must be done by the admin
+        # of said experiment.
+        if request.POST.get("publish", None):
+            scoreset.publish()
+            send_admin_email(request.user, scoreset)
+
+        user = request.user
+        scoreset.created_by = user
+        scoreset.last_edit_by = user
+        scoreset.update_last_edit_info(user)
+        save_and_create_revision_if_tracked_changed(user, scoreset)
+
+        assign_user_as_instance_admin(user, scoreset)
+        accession = scoreset.accession
+        return redirect("scoreset:scoreset_detail", accession=accession)
+
+    else:
+        context["scoreset_form"] = scoreset_form
+        return render(
+            request,
+            "scoreset/new_scoreset.html",
             context=context
         )
