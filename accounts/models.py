2a5a08a14e470db069a6426f006d39af2ec54c14
daniel <danielce90@gmail.com>
2018-03-20 18:00:55 +1100

    import fixes

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,8 @@
 
-from django.contrib.auth.models import Group, User
-from django.contrib.auth.models import AnonymousUser
-from django.core.exceptions import ObjectDoesNotExist
-
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from django.utils.html import format_html
 
 from experiment.models import ExperimentSet, Experiment, ScoreSet
