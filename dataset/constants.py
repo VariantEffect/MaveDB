28e3de586bbe26a3c4c92cc7d8963c2e26d84e50
daniel <danielce90@gmail.com>
2018-03-26 19:26:44 +1100

    Added pattern constants for url pattern matching on URN models

--- dataset/constants.py
+++ dataset/constants.py
@@ -2,6 +2,12 @@
 Constant definitions for application `experiment`.
 """
 
+from urn.validators import (
+    MAVEDB_EXPERIMENTSET_URN_PATTERN,
+    MAVEDB_EXPERIMENT_URN_PATTERN,
+    MAVEDB_SCORESET_URN_PATTERN
+)
+
 nan_col_values = set(['nan', 'na', 'none', '', ' ', 'NaN', 'None', 'Na', 'NA'])
 hgvs_column = "hgvs"
 score_columns = "score_columns"
@@ -11,3 +17,10 @@ variant_score_data = 'score_data'
 variant_count_data = 'count_data'
 required_score_column = 'score'
 variant_metadata = 'metadata'
+
+experimentset_url_pattern = MAVEDB_EXPERIMENTSET_URN_PATTERN[1:-1]
+experiment_url_pattern = MAVEDB_EXPERIMENT_URN_PATTERN[1:-1]
+scoreset_url_pattern = MAVEDB_SCORESET_URN_PATTERN[1:-1]
+any_url_pattern = '|'.join([
+    experimentset_url_pattern, experiment_url_pattern, scoreset_url_pattern
+])
