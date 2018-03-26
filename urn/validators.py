a2d26333d05204b0f2020b97b931764668f8bb62
daniel <danielce90@gmail.com>
2018-03-26 19:18:14 +1100

    Moved re/pattern definitons to the top of the file

--- urn/validators.py
+++ urn/validators.py
@@ -2,6 +2,46 @@ import re
 
 from django.core.exceptions import ValidationError
 
+MAVEDB_EXPERIMENTSET_URN_DIGITS = 8
+MAVEDB_URN_MAX_LENGTH = 64
+MAVEDB_URN_NAMESPACE = "mavedb"
+
+# Experimentset Pattern/Compiled RE
+MAVEDB_EXPERIMENTSET_URN_PATTERN = r'^urn:{namespace}:\d{{{width}}}$'.format(
+    namespace=MAVEDB_URN_NAMESPACE,
+    width=MAVEDB_EXPERIMENTSET_URN_DIGITS
+)
+MAVEDB_EXPERIMENTSET_URN_RE = re.compile(MAVEDB_EXPERIMENTSET_URN_PATTERN)
+
+# Experiment Pattern/Compiled RE
+MAVEDB_EXPERIMENT_URN_PATTERN = r'{pattern}-[a-z]+$'.format(
+    pattern=MAVEDB_EXPERIMENTSET_URN_PATTERN[:-1]
+)
+MAVEDB_EXPERIMENT_URN_RE = re.compile(MAVEDB_EXPERIMENT_URN_PATTERN)
+
+# Scoreset Pattern/Compiled RE
+MAVEDB_SCORESET_URN_PATTERN = r'{pattern}-\d+$'.format(
+    pattern=MAVEDB_EXPERIMENT_URN_PATTERN[:-1]
+)
+MAVEDB_SCORESET_URN_RE = re.compile(MAVEDB_SCORESET_URN_PATTERN)
+
+# Variant Pattern/Compiled RE
+MAVEDB_VARIANT_URN_PATTERN = r'{pattern}#\d+$'.format(
+    pattern=MAVEDB_SCORESET_URN_PATTERN[:-1]
+)
+MAVEDB_VARIANT_URN_RE = re.compile(MAVEDB_VARIANT_URN_PATTERN)
+
+# Any Pattern/Compiled RE
+MAVEDB_ANY_URN_PATTERN = '|'.join([r'({pattern})'.format(pattern=p) for p in (
+    MAVEDB_EXPERIMENTSET_URN_PATTERN,
+    MAVEDB_EXPERIMENT_URN_PATTERN,
+    MAVEDB_SCORESET_URN_PATTERN,
+    MAVEDB_VARIANT_URN_PATTERN)
+])
+MAVEDB_ANY_URN_RE = re.compile(MAVEDB_ANY_URN_PATTERN)
+
+
+
 
 def validate_mavedb_urn(urn):
     if not MAVEDB_ANY_URN_RE.match(urn):
@@ -41,32 +81,3 @@ def validate_mavedb_urn_variant(urn):
             "%(urn)s is not a valid Variant urn.",
             params={"urn": urn}
         )
-
-
-MAVEDB_EXPERIMENTSET_URN_DIGITS = 8
-MAVEDB_URN_MAX_LENGTH = 64
-MAVEDB_URN_NAMESPACE = "mavedb"
-MAVEDB_EXPERIMENTSET_URN_PATTERN = r'^urn:{namespace}:\d{{{width}}}$'.format(
-    namespace=MAVEDB_URN_NAMESPACE,
-    width=MAVEDB_EXPERIMENTSET_URN_DIGITS
-)
-MAVEDB_EXPERIMENTSET_URN_RE = re.compile(MAVEDB_EXPERIMENTSET_URN_PATTERN)
-MAVEDB_EXPERIMENT_URN_PATTERN = r'{pattern}-[a-z]+$'.format(
-    pattern=MAVEDB_EXPERIMENTSET_URN_PATTERN[:-1]
-)
-MAVEDB_EXPERIMENT_URN_RE = re.compile(MAVEDB_EXPERIMENT_URN_PATTERN)
-MAVEDB_SCORESET_URN_PATTERN = r'{pattern}-\d+$'.format(
-    pattern=MAVEDB_EXPERIMENT_URN_PATTERN[:-1]
-)
-MAVEDB_SCORESET_URN_RE = re.compile(MAVEDB_SCORESET_URN_PATTERN)
-MAVEDB_VARIANT_URN_PATTERN = r'{pattern}#\d+$'.format(
-    pattern=MAVEDB_SCORESET_URN_PATTERN[:-1]
-)
-MAVEDB_VARIANT_URN_RE = re.compile(MAVEDB_VARIANT_URN_PATTERN)
-MAVEDB_ANY_URN_PATTERN = '|'.join([r'({pattern})'.format(pattern=p) for p in (
-    MAVEDB_EXPERIMENTSET_URN_PATTERN,
-    MAVEDB_EXPERIMENT_URN_PATTERN,
-    MAVEDB_SCORESET_URN_PATTERN,
-    MAVEDB_VARIANT_URN_PATTERN)
-])
-MAVEDB_ANY_URN_RE = re.compile(MAVEDB_ANY_URN_PATTERN)
