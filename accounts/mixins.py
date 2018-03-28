394c033514ae5d84b9e2f3cbef888010194e013e
Daniel Esposito <danielce90@gmail.com>
2018-03-28 13:46:41 +1100

    Changed 'author(s)' to 'contributor(s)'

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -29,23 +29,23 @@ class GroupPermissionMixin(object):
         viewers = [u.pk for u in users if user_is_viewer_for_instance(u, self)]
         return User.objects.filter(pk__in=viewers).exclude(is_superuser=True)
 
-    def get_author_models(self):
+    def get_contributor_models(self):
         return list(contributors_for_instance(self))
 
-    def get_authors(self, string=True):
+    def get_contributors(self, string=True):
         authors = [
             u.profile.get_short_name()
             for u in contributors_for_instance(self)
         ]
         return ', '.join(authors) if string else authors
 
-    def get_authors_by_full_name(self, string=True):
+    def get_contributors_by_full_name(self, string=True):
         authors = [
             u.profile.get_full_name()
             for u in contributors_for_instance(self)
         ]
         return ', '.join(authors) if string else authors
 
-    def get_authors_by_username(self, string=True):
+    def get_contributors_by_username(self, string=True):
         authors = [u.username for u in contributors_for_instance(self)]
         return ', '.join(authors) if string else authors
