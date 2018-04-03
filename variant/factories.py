a40a76120b7059541ecb4045692beb22f18f26c8
Daniel Esposito <danielce90@gmail.com>
2018-04-03 20:26:45 +1000

    Created factory c

---
+++ variant/factories.py
@@ -0,0 +1,44 @@
+"""
+variant.factories
+=================
+
+This module contains factory methods for creating test fixtures. If there are
+any updates to the models which will have an impact on the tests, then they
+can be changed once here instead of throughout all the tests. This will help
+with future maintainability.
+"""
+from random import choice
+
+import factory
+import factory.fuzzy
+from factory.django import DjangoModelFactory
+
+import dataset.constants as constants
+from dataset.factories import ScoreSetFactory
+
+from .models import Variant
+
+
+sample_hgvs = ['c.101G>C', 'c.10C>A', 'c.41G>A', 'c.53G>T', 'c.17C>G']
+
+
+def generate_hgvs():
+    """Generates a random hgvs string from a small sample."""
+    return choice(sample_hgvs)
+
+
+class VariantFactory(DjangoModelFactory):
+    """
+    Factory for producing test instances for :class:`Variant`.
+    """
+    class Meta:
+        model = Variant
+
+    urn = None
+    scoreset = factory.SubFactory(ScoreSetFactory)
+    hgvs = factory.fuzzy.FuzzyChoice(sample_hgvs)
+    data = {
+        constants.variant_score_data: {constants.required_score_column: 1.0},
+        constants.variant_count_data: {},
+        constants.variant_metadata: {}
+    }
