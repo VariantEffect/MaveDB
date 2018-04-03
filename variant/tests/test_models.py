a08b1b857ff496df9d5dd8ceb2ddc08b6fb45568
Daniel Esposito <danielce90@gmail.com>
2018-04-03 20:30:42 +1000

    Completed test suite for Variant app

--- variant/tests/test_models.py
+++ variant/tests/test_models.py
@@ -1,126 +1,76 @@
-# import datetime
-#
-# from django.db import IntegrityError
-# from django.contrib.auth.models import Group
-# from django.core.exceptions import ValidationError
-# from django.test import TransactionTestCase
-# from django.db.models import ProtectedError
-#
-# from metadata.models import (
-#     Keyword, SraIdentifier, DoiIdentifier, PubmedIdentifier
-# )
-# from variant.models import Variant
-# from ..models import Experiment, ExperimentSet, ScoreSet
-# import dataset.constants as constants
-#
-#
-# class TestVariant(TransactionTestCase):
-#     """
-#     The purpose of this unit test is to test that the database model
-#     :py:class:`ScoreSet`, representing an experiment with associated
-#     :py:class:`Variant` objects. We will test correctness of creation,
-#     validation, uniqueness, queries and that the appropriate errors are raised.
-#     """
-#
-#     reset_sequences = True
-#
-#     def setUp(self):
-#         self.target = "target"
-#         self.wt_seq = "ATCG"
-#         self.exp = Experiment.objects.create(
-#             target=self.target,
-#             wt_sequence=self.wt_seq
-#         )
-#         self.scs_1 = ScoreSet.objects.create(experiment=self.exp)
-#         self.scs_2 = ScoreSet.objects.create(experiment=self.exp)
-#
-#         self.hgvs_1 = "c.45C>G"
-#         self.hgvs_2 = "c.1A>T"
-#         self.var_accession_11 = "SCSVAR000001A.1.1"
-#         self.var_accession_12 = "SCSVAR000001A.1.2"
-#         self.var_accession_21 = "SCSVAR000001A.2.1"
-#         self.var_accession_22 = "SCSVAR000001A.2.2"
-#
-#     def test_can_create_minimal_variant(self):
-#         Variant.objects.create(scoreset=self.scs_1, hgvs=self.hgvs_1)
-#         var = Variant.objects.all()[0]
-#         self.assertEqual(var.accession, self.var_accession_11)
-#         self.assertEqual(var.scoreset, self.scs_1)
-#
-#     def test_autoassign_accession_in_variant(self):
-#         Variant.objects.create(scoreset=self.scs_1, hgvs=self.hgvs_1)
-#         Variant.objects.create(scoreset=self.scs_1, hgvs=self.hgvs_2)
-#
-#         var_1 = Variant.objects.all()[0]
-#         var_2 = Variant.objects.all()[1]
-#         self.assertEqual(var_1.accession, self.var_accession_11)
-#         self.assertEqual(var_2.accession, self.var_accession_12)
-#
-#     def test_cannot_create_variant_with_duplicate_accession(self):
-#         Variant.objects.create(
-#             scoreset=self.scs_1,
-#             hgvs=self.hgvs_1
-#         )
-#         with self.assertRaises(IntegrityError):
-#             Variant.objects.create(
-#                 accession=self.var_accession_11,
-#                 hgvs=self.hgvs_1,
-#                 scoreset=self.scs_1
-#             )
-#
-#     def test_cannot_save_without_scoreset(self):
-#         with self.assertRaises(IntegrityError):
-#             Variant.objects.create(
-#                 accession=self.var_accession_11,
-#                 hgvs=self.hgvs_1
-#             )
-#         with self.assertRaises(IntegrityError):
-#             Variant.objects.create(hgvs=self.hgvs_1)
-#
-#     def test_cannot_save_without_hgvs(self):
-#         with self.assertRaises(IntegrityError):
-#             Variant.objects.create(
-#                 scoreset=self.scs_1
-#             )
-#
-#     def test_validation_error_json_no_scores_key(self):
-#         var = Variant.objects.create(
-#             scoreset=self.scs_1,
-#             hgvs=self.hgvs_1,
-#             data={"not scores": {}, COUNTS_KEY: {}}
-#         )
-#         with self.assertRaises(ValidationError):
-#             var.full_clean()
-#
-#     def test_validation_error_json_no_counts_key(self):
-#         var = Variant.objects.create(
-#             scoreset=self.scs_1,
-#             hgvs=self.hgvs_1,
-#             data={SCORES_KEY: {}, "not counts": {}}
-#         )
-#         with self.assertRaises(ValidationError):
-#             var.full_clean()
-#
-#     def test_variants_sorted_by_most_recent(self):
-#         date_1 = datetime.date.today()
-#         date_2 = datetime.date.today() + datetime.timedelta(days=1)
-#         Variant.objects.create(
-#             scoreset=self.scs_1,
-#             hgvs=self.hgvs_1,
-#             creation_date=date_1
-#         )
-#         Variant.objects.create(
-#             scoreset=self.scs_1,
-#             hgvs=self.hgvs_1,
-#             creation_date=date_2
-#         )
-#         self.assertEqual(
-#             self.var_accession_12, Variant.objects.all()[0].accession)
-#
-#     def test_new_variant_has_todays_date_by_default(self):
-#         Variant.objects.create(
-#             scoreset=self.scs_1,
-#             hgvs=self.hgvs_1,
-#         )
-#         var = Variant.objects.all()[0]
-#         self.assertEqual(var.creation_date, datetime.date.today())
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+import dataset.constants as constants
+from dataset.factories import ScoreSetFactory
+
+from ..factories import VariantFactory
+
+
+class TestVariant(TestCase):
+    """
+    The purpose of this unit test is to test that the database model
+    :py:class:`ScoreSet`, representing an experiment with associated
+    :py:class:`Variant` objects. We will test correctness of creation,
+    validation, uniqueness, queries and that the appropriate errors are raised.
+    """
+
+    def test_autoassign_urn_when_save(self):
+        variant = VariantFactory()
+        scoreset = variant.scoreset
+        urn = "{}#{}".format(scoreset.urn, scoreset.last_child_value)
+        self.assertEqual(variant.urn, urn)
+
+    def test_create_urn_increments_last_child_value_by_one(self):
+        scoreset = ScoreSetFactory()
+        before_variant = scoreset.last_child_value
+        _ = VariantFactory(scoreset=scoreset)
+        after_variant = scoreset.last_child_value
+        self.assertEqual(before_variant + 1, after_variant)
+
+    def test_cannot_create_variant_with_duplicate_urn(self):
+        variant = VariantFactory()
+        with self.assertRaises(IntegrityError):
+            _ = VariantFactory(urn=variant.urn)
+
+    def test_cannot_save_without_scoreset(self):
+        with self.assertRaises(IntegrityError):
+            _ = VariantFactory(scoreset=None)
+
+    def test_cannot_save_without_hgvs(self):
+        with self.assertRaises(IntegrityError):
+            _ = VariantFactory(hgvs=None)
+
+    def test_validation_error_json_has_no_scores_key(self):
+        var = VariantFactory(data={
+            constants.variant_count_data: {},
+            constants.variant_metadata: {}
+        })
+        with self.assertRaises(ValidationError):
+            var.full_clean()
+
+    def test_validation_error_json_has_no_counts_key(self):
+        var = VariantFactory(data={
+            constants.variant_score_data: {constants.required_score_column: 1},
+            constants.variant_metadata: {}
+        })
+        with self.assertRaises(ValidationError):
+            var.full_clean()
+
+    def test_validation_error_json_has_no_meta_key(self):
+        var = VariantFactory(data={
+            constants.variant_count_data: {},
+            constants.variant_score_data: {constants.required_score_column: 1}
+        })
+        with self.assertRaises(ValidationError):
+            var.full_clean()
+
+    def test_validation_error_data_missing_required_score_column(self):
+        var = VariantFactory(data={
+            constants.variant_metadata: {},
+            constants.variant_count_data: {},
+            constants.variant_score_data: {}
+        })
+        with self.assertRaises(ValidationError):
+            var.full_clean()
---
+++ variant/tests/test_validators.py
@@ -0,0 +1,242 @@
+from io import BytesIO
+
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+import dataset.constants as constants
+from dataset.constants import hgvs_column, required_score_column
+
+from ..factories import generate_hgvs, VariantFactory
+from ..validators import (
+    validate_hgvs_string,
+    validate_scoreset_columns_match_variant,
+    validate_variant_json,
+    validate_variant_rows
+)
+
+
+class TestValidateMatchingColumns(TestCase):
+    """
+    Tests the function :func:`validate_scoreset_columns_match_variant` which
+    throws a `ValidationError` if the keys of a variant's data do not match
+    the corresponding columns defined in the parent
+    :class:`dataset.models.scoreset.ScoreSet`.
+    """
+    def test_validationerror_non_matching_score_columns(self):
+        variant = VariantFactory()
+        with self.assertRaises(ValidationError):
+            variant.data[constants.variant_score_data] = {}
+            validate_scoreset_columns_match_variant(variant.scoreset, variant)
+
+    def test_validationerror_non_matching_meta_columns(self):
+        variant = VariantFactory()
+        with self.assertRaises(ValidationError):
+            variant.data[constants.variant_metadata] = {'meta': 'variant'}
+            validate_scoreset_columns_match_variant(variant.scoreset, variant)
+
+    def test_validationerror_non_matching_count_columns(self):
+        variant = VariantFactory()
+        with self.assertRaises(ValidationError):
+            variant.data[constants.variant_count_data] = {'count': 1}
+            validate_scoreset_columns_match_variant(variant.scoreset, variant)
+
+    def test_compares_sorted_columns(self):
+        variant = VariantFactory()
+        variant.data[constants.variant_score_data] = {
+            'other': 1, constants.required_score_column: 1}
+        variant.scoreset.dataset_columns[constants.score_columns] = [
+            constants.required_score_column, 'other']
+        # This should pass
+        validate_scoreset_columns_match_variant(variant.scoreset, variant)
+
+
+class TestHGVSValidator(TestCase):
+    """
+    Tests the function :func:`validate_hgvs_string` to see if it is able
+    to validate strings which do not comply with the HGVS standard for
+    coding, non-coding and nucleotide variants and multi-variants.
+    """
+    pass
+
+
+class TestVariantJsonValidator(TestCase):
+    """
+    Tests the validator :func:`validate_variant_json` to check if the correct
+    errors are thrown if an incorrectly formatted `dictionary` is set
+    as a the `data` `JSONField` attribute of a :class:`..models.Variant`
+    instance.
+    """
+    def test_validationerror_missing_score_data_key(self):
+        data = {
+            constants.variant_count_data: {},
+            constants.variant_metadata: {}
+        }
+        with self.assertRaises(ValidationError):
+            validate_variant_json(data)
+
+    def test_validationerror_missing_count_data_key(self):
+        data = {
+            constants.variant_score_data: {},
+            constants.variant_metadata: {}
+        }
+        with self.assertRaises(ValidationError):
+            validate_variant_json(data)
+
+    def test_validationerror_missing_meta_data_key(self):
+        data = {
+            constants.variant_count_data: {},
+            constants.variant_metadata: {}
+        }
+        with self.assertRaises(ValidationError):
+            validate_variant_json(data)
+
+    def test_validationerror_constains_unexpected_keys(self):
+        data = {
+            'extra': {},
+            constants.variant_score_data: {},
+            constants.variant_count_data: {},
+            constants.variant_metadata: {}
+        }
+        with self.assertRaises(ValidationError):
+            validate_variant_json(data)
+
+    def test_validationerror_values_not_dict(self):
+        data = {
+            constants.variant_score_data: {},
+            constants.variant_count_data: {},
+            constants.variant_metadata: {}
+        }
+        for key in data.keys():
+            data[key] = []
+            with self.assertRaises(ValidationError):
+                validate_variant_json(data)
+            data[key] = {}
+
+
+class TestVariantRowValidator(TestCase):
+    """
+    Tests the validator :func:`validate_variant_rows` to check if the correct
+    errors are thrown when invalid rows are encountered in a
+    scores/counts/meta data input file. Checks for:
+        - Invalid HGVS string in a row
+        - Row HGVS is defined in more than one row
+        - Row values are not int/float for a count/score file
+
+    Tests also check to see if the correct header and hgvs data information
+    is parsed and returned.
+    """
+    def test_validationerror_row_hgvs_is_not_a_string(self):
+        data = "{},{}\n,1.0".format(hgvs_column, required_score_column)
+        with self.assertRaises(ValidationError):
+            validate_variant_rows(BytesIO(data.encode()))
+
+    def test_validationerror_missing_hgvs_column(self):
+        data = "{},{}\n{},1.0".format(
+            'not_hgvs', required_score_column, generate_hgvs())
+        with self.assertRaises(ValidationError):
+            validate_variant_rows(BytesIO(data.encode()))
+
+    def test_strip_whitespace_from_header_columns(self):
+        data = " {} , {} \n{},1.0".format(
+            hgvs_column, required_score_column, generate_hgvs())
+        header, _ = validate_variant_rows(BytesIO(data.encode()))
+        self.assertEqual(header, [required_score_column])
+
+    def test_validationerror_null_values_in_column(self):
+        for value in constants.nan_col_values:
+            data = "{},{}\n{},1.0".format(hgvs_column, value, generate_hgvs())
+            with self.assertRaises(ValidationError):
+                validate_variant_rows(BytesIO(data.encode()))
+
+    def test_validationerror_less_than_two_header_columns(self):
+        data = "{}\n{}".format(hgvs_column, generate_hgvs())
+        with self.assertRaises(ValidationError):
+            validate_variant_rows(BytesIO(data.encode()))
+
+    def test_validationerror_no_variants_parsed(self):
+        data = "{},{}\n".format(hgvs_column, required_score_column)
+        with self.assertRaises(ValidationError):
+            validate_variant_rows(BytesIO(data.encode()))
+
+    def test_validationerror_invalid_hgvs(self):
+        self.fail("Write this when you have written the hgvs validator.")
+
+    def test_validationerror_non_numeric_values_in_non_meta_input(self):
+        data = "{},{}\n{},hello world".format(
+            hgvs_column, required_score_column, generate_hgvs())
+        with self.assertRaises(ValidationError):
+            validate_variant_rows(BytesIO(data.encode()))
+
+    def test_validationerror_same_hgvs_defined_in_two_rows(self):
+        hgvs = generate_hgvs()
+        data = "{},{}\n{},1.0\n{},1.0".format(
+            hgvs_column, required_score_column, hgvs, hgvs)
+        with self.assertRaises(ValidationError):
+            validate_variant_rows(BytesIO(data.encode()))
+
+    def test_null_values_converted_to_None(self):
+        hgvs = generate_hgvs()
+        for value in constants.nan_col_values:
+            data = "{},{}\n{},{}".format(
+                hgvs_column, required_score_column, hgvs, value)
+            header, hgvs_map = validate_variant_rows(BytesIO(data.encode()))
+            expected_map = {hgvs: {required_score_column: None}}
+            expected_header = [required_score_column]
+            self.assertEqual(header, expected_header)
+            self.assertEqual(dict(hgvs_map), expected_map)
+
+    def test_default_dict_fills_new_keys_with_None_col_values(self):
+        hgvs = generate_hgvs()
+        data = "{},{}\n{},1.0".format(hgvs_column, required_score_column, hgvs)
+        _, hgvs_map = validate_variant_rows(BytesIO(data.encode()))
+        expected_map = {hgvs: {required_score_column: 1.0}}
+        self.assertEqual(dict(hgvs_map), expected_map)
+
+        # Index a new value to create the default entry
+        row = hgvs_map['new_hgvs']
+        self.assertEqual(row, {required_score_column: None})
+
+    def test_trailing_whitespace_stripped_from_column_values(self):
+        value = ' hello world '
+        hgvs = generate_hgvs()
+        data = "{},meta\n{},{}".format(hgvs_column, hgvs, value)
+        _, hgvs_map = validate_variant_rows(
+            BytesIO(data.encode()), is_meta=True)
+        expected_map = {hgvs: {'meta': value.strip()}}
+        self.assertEqual(dict(hgvs_map), expected_map)
+
+    def test_returns_sorted_header(self):
+        hgvs = generate_hgvs()
+        data = "{},a,{}\n{},1.0,1.0".format(
+            hgvs_column, required_score_column, hgvs)
+        header, _ = validate_variant_rows(BytesIO(data.encode()))
+        expected = [required_score_column, 'a']
+        self.assertEqual(header, expected)
+
+    def test_hgvs_column_filtered_from_row_data(self):
+        hgvs = generate_hgvs()
+        data = "{},{}\n{},1.0".format(hgvs_column, required_score_column, hgvs)
+        header, hgvs_map = validate_variant_rows(BytesIO(data.encode()))
+        self.assertEqual(list(hgvs_map[hgvs].keys()), [required_score_column])
+        self.assertEqual(header, [required_score_column])
+
+    def test_parses_non_meta_column_values_to_float(self):
+        hgvs = generate_hgvs()
+        data = "{},{}\n{},1.0".format(hgvs_column, required_score_column, hgvs)
+        _, hgvs_map = validate_variant_rows(BytesIO(data.encode()))
+        value = hgvs_map[hgvs][required_score_column]
+        self.assertIsInstance(value, float)
+
+    def test_does_not_split_double_quoted_variants(self):
+        hgvs = '{},{}'.format(generate_hgvs(), generate_hgvs())
+        data = '{},{}\n"{}",1.0'.format(
+            hgvs_column, required_score_column, hgvs)
+        _, hgvs_map = validate_variant_rows(BytesIO(data.encode()))
+        self.assertIn(hgvs, hgvs_map)
+
+    def test_validationerror_malformed_row(self):
+        hgvs = '{},{}'.format(generate_hgvs(), generate_hgvs())
+        data = "{},{}\n'{}',1.0".format(
+            hgvs_column, required_score_column, hgvs)
+        with self.assertRaises(ValidationError):
+            _ = validate_variant_rows(BytesIO(data.encode()))
