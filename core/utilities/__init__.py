f95574f3515053465f41b9f4cdc6bc038d4f50b8
daniel <danielce90@gmail.com>
2018-04-10 16:23:54 +1000

    main utilities to core

--- core/utilities/__init__.py
+++ core/utilities/__init__.py
@@ -1,4 +1,5 @@
 import dataset.constants as constants
 
 def is_null(value):
+    """Returns True if a stripped/lowercase value in in `nan_col_values`."""
     return str(value).strip().lower() in constants.nan_col_values
