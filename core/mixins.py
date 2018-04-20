9ecd1fd23be48d2bbf062672584287f4f0741420
daniel <danielce90@gmail.com>
2018-04-20 16:29:22 +1000

    Mixins FilterMixin and SearchMixin for general queryset filtering on any model

--- core/mixins.py
+++ core/mixins.py
@@ -1,82 +1,132 @@
-from django.views.generic.base import ContextMixin
+import logging
 
-from django.http.response import HttpResponseRedirect
+from django.db.models import Q
+
+logger = logging.getLogger('django')
 
 
-class MultiFormMixin(ContextMixin):
+class FilterMixin(object):
     """
-    Mixin allowing multiple forms to be used in a class-based form view by
-    providing functionality to handle each form separetely.
-
-    You will need to implement the following for each key in `form_classes`:
-        - <key>_form_valid
-        - <key>_form_invalid
-
-    The following are optional:
-        - get_<key>_initial
-        - create_<key>_form
+    Mixin enabling the creation of complex search queries using
+    AND/OR `Q` objects.
     """
-    form_classes = {}
-    prefixes = {}
-    success_urls = {}
-    grouped_forms = {}
-
-    initial = {}
-    prefix = None
-    success_url = None
-
-    def get_form_classes(self):
-        return self.form_classes
-
-    def get_forms(self, form_classes, form_names=None, bind_all=False):
-        return dict([(key, self._create_form(key, klass, (
-                    form_names and key in form_names) or bind_all)) \
-                     for key, klass in form_classes.items()])
-
-    def get_form_kwargs(self, form_name, bind_form=False):
-        kwargs = {}
-        kwargs.update({'initial': self.get_initial(form_name)})
-        kwargs.update({'prefix': self.get_prefix(form_name)})
-
-        if bind_form:
-            kwargs.update(self._bind_form_data())
-
-        return kwargs
-
-    def forms_valid(self, forms, form_name):
-        form_valid_method = '%s_form_valid' % form_name
-        if hasattr(self, form_valid_method):
-            return getattr(self, form_valid_method)(forms[form_name])
+    def search_to_q(self, search_value, field_name, filter_type):
+        if isinstance(search_value, (list, set)):
+            search_value = list(search_value)
+            if len(search_value) == 1:
+                return self.value_to_q(search_value[0], field_name, filter_type)
+            return self.list_to_or_q(search_value, field_name, filter_type)
         else:
-            return HttpResponseRedirect(self.get_success_url(form_name))
+            return self.value_to_q(search_value, field_name, filter_type)
 
-    def forms_invalid(self, forms):
-        return self.render_to_response(self.get_context_data(forms=forms))
+    def list_to_or_q(self, search_list, field_name, filter_type):
+        q_object = Q()
+        if not len(search_list):
+            return q_object
+        for search_value in search_list:
+            q_object |= self.value_to_q(search_value, field_name, filter_type)
+        return q_object
 
-    def get_initial(self, form_name):
-        initial_method = 'get_%s_initial' % form_name
-        if hasattr(self, initial_method):
-            return getattr(self, initial_method)()
+    def list_to_and_q(self, search_list, field_name, filter_type):
+        q_object = Q()
+        for search_value in search_list:
+            q_object &= self.value_to_q(search_value, field_name, filter_type)
+        return q_object
+
+    @staticmethod
+    def value_to_q(value, field_name, filter_type):
+        value = str(value).strip()
+        if not value:
+            return Q()
+        return Q(**{"%s__%s" % (field_name, filter_type): value})
+
+    @staticmethod
+    def and_join_qs(qs):
+        joined = Q()
+        for q in qs:
+            if len(q) > 0:
+                joined &= q
+        return joined
+
+    @staticmethod
+    def or_join_qs(qs):
+        joined = Q()
+        for q in qs:
+            if len(q) > 0:
+                joined |= q
+        return joined
+
+
+class SearchMixin(FilterMixin):
+    """
+    Provides functionality to search multiple fields in a model, and combine
+    them with either OR or AND. The entry point for creating a search `Q` is
+    `search_all`.
+
+    You can provide this function with a string to perform a general
+    search on all model fields defined in `search_field_to_function`,
+    or a dictionary mapping the keys in `search_field_to_function` to a value.
+    You will also need to specify the `join_func` parameter to indicate how
+    to join the Qs. If no function is provided, a list of un-joined `Q`s will
+    be returned.
+
+    For each key defined in `search_field_to_function`, a matching
+    function will need to be defined which returns a `Q`. Example:
+
+    def filter_abstract(self, value):
+        return self.search_to_q(
+            search_value=value,
+            field_name='abstract_text',
+            filter_type='icontains',
+        )
+
+    You can specify how to construct a `Q` by providing a
+    `field_name` to search a model with and a `filter_type` to `search_to_q`.
+    `filter_types` are those supported by a `QuerySet`. ALternatively, you can
+    define a more complex function returning a `Q`.
+
+    `search_field_to_model_field` is currently unused, but define it anyway
+    for future compatibility.
+    """
+    @staticmethod
+    def search_field_to_model_field():
+        raise NotImplementedError
+
+    @staticmethod
+    def search_field_to_function():
+        raise NotImplementedError
+
+    def get_function_for_field(self, field):
+        if field not in self.search_field_to_function():
+            logging.warning(
+                "Could not find filter function for field {}.".format(field)
+            )
+            return None
+        return self.search_field_to_function()[field]
+
+    def q_object_list(self, dict_or_str):
+        return self.search_all(dict_or_str, join_func=None)
+
+    def and_q_object(self, dict_or_str):
+        return self.search_all(dict_or_str, join_func=self.and_join_qs)
+
+    def or_q_object(self, dict_or_str):
+        return self.search_all(dict_or_str, join_func=self.or_join_qs)
+
+    def search_all(self, value, join_func=None):
+        functions = self.search_field_to_function()
+        if isinstance(value, dict):
+            qs = []
+            for field, value in value.items():
+                search_func = self.get_function_for_field(field)
+                if search_func is not None:
+                    qs.append(search_func(value))
+        elif isinstance(value, str):
+            qs = [func(value) for _, func in functions.items()]
         else:
-            return self.initial.copy()
-
-    def get_prefix(self, form_name):
-        return self.prefixes.get(form_name, self.prefix)
-
-    def get_success_url(self, form_name=None):
-        return self.success_urls.get(form_name, self.success_url)
-
-    def _create_form(self, form_name, klass, bind_form):
-        form_kwargs = self.get_form_kwargs(form_name, bind_form)
-        form_create_method = 'create_%s_form' % form_name
-        if hasattr(self, form_create_method):
-            form = getattr(self, form_create_method)(**form_kwargs)
-        else:
-            form = klass(**form_kwargs)
-        return form
-
-    def _bind_form_data(self):
-        if self.request.method in ('POST', 'PUT'):
-            return {'data': self.request.POST,
-                    'files': self.request.FILES, }
-        return {}
+            raise TypeError("Expected `dict` or `str`. Found {}".format(
+                type(value).__name__
+            ))
+        if join_func is None:
+            return qs
+        return join_func(qs)
