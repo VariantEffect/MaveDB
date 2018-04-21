25d8975937535b5156107f96e7d34ef2bfe8a0a8
Daniel Esposito <danielce90@gmail.com>
2018-04-21 18:37:38 +1000

    Sortable table rows

--- main/static/main/main/js/base.js
+++ main/static/main/main/js/base.js
@@ -341,3 +341,66 @@ $("#remove_interval").click(function () {
     reset_index("#interval");
     remove_formset("#interval");
 });
+
+
+function sortTable(id, n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  var x_val, y_val = 0;
+  table = document.getElementById(id);
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      x_val = parseFloat(x.innerHTML.toLowerCase());
+      y_val = parseFloat(y.innerHTML.toLowerCase());
+      if (isNaN(x_val) || isNaN(y_val)) {
+          x_val = x.innerHTML.toLowerCase();
+          y_val = y.innerHTML.toLowerCase();
+      }
+      if (dir === "asc") {
+        if (x_val > y_val) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir === "desc") {
+        if (x_val < y_val) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
--- search/views.py
+++ search/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from dataset.models.experiment import Experiment
 from accounts.permissions import user_is_anonymous
@@ -16,40 +15,9 @@ def search_view(request):
         if form.is_valid():
             experiments = form.query_experiments()
 
-    if user_is_anonymous(request.user):
-        experiments = [e for e in experiments if not e.private]
-        has_data = len(experiments) > 0
-    else:
-        users_experiments = request.user.profile.contributor_experiments()
-        private_experiments = [
-            e for e in experiments if e.private and e in users_experiments
-        ]
-        public_experiments = [e for e in experiments if not e.private]
-        has_data = len(private_experiments) > 0 or len(public_experiments) > 0
-
-    # Handle the pagination request options
-    try:
-        per_page = request.GET.get('per-page', 25)
-        per_page = int(per_page)
-    except:
-        per_page = 25
-
-    paginator = Paginator(experiments, per_page=per_page)
-    try:
-        page_num = request.GET.get('page', 1)
-    except PageNotAnInteger:
-        page_num = 1
-    except EmptyPage:
-        page_num = paginator.num_pages
-
-    experiments = paginator.page(page_num)
     context = {
         "form": form,
-        "contributor_experiments": experiments,
-        "has_data": has_data,
-        "per_page": per_page,
-        "page_num": page_num,
-        "per_page_selections": [25, 50, 100]
+        "experiments": experiments,
     }
 
     return render(request, "search/search.html", context)
