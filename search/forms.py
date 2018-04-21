1194d8b9c89bd708ac97126bb25befdd7d9a3311
Daniel Esposito <danielce90@gmail.com>
2018-04-21 22:55:56 +1000

    UI updates

--- search/forms.py
+++ search/forms.py
@@ -49,12 +49,6 @@ class SearchForm(forms.Form):
     #         attrs={"class": "form-control"}
     #     )
     # )
-    contributors = forms.CharField(
-        max_length=None, label="Contributors", required=False,
-        widget=forms.widgets.TextInput(
-            attrs={"class": "form-control"}
-        )
-    )
 
     def clean_urns(self):
         return filter_empty(
@@ -79,21 +73,6 @@ class SearchForm(forms.Form):
     def clean_keywords(self):
         return filter_empty(
             parse_query(self.cleaned_data.get("keywords", ""))
-        )
-
-    def clean_targets(self):
-        return filter_empty(
-            parse_query(self.cleaned_data.get("targets", ""))
-        )
-
-    def clean_target_organisms(self):
-        return filter_empty(
-            parse_query(self.cleaned_data.get("target_organisms", ""))
-        )
-
-    def clean_contributors(self):
-        return filter_empty(
-            parse_query(self.cleaned_data.get("contributors", ""))
         )
 
     def clean(self):
@@ -128,16 +107,6 @@ class SearchForm(forms.Form):
             return entries
         return None
 
-    def search_by_target(self, model, targets):
-        if targets:
-            entries = model.objects.none()
-            for target in targets:
-                entries |= model.objects.all().filter(
-                    target__icontains=target
-                )
-            return entries
-        return None
-
     def search_by_metadata(self, model, metadata_tags):
         if metadata_tags:
             entries = model.objects.none()
@@ -147,44 +116,6 @@ class SearchForm(forms.Form):
                 entries |= model.objects.all().filter(
                     method_text__icontains=tag
                 )
-            return entries
-        return None
-
-    def search_by_target_organism(self, target_organisms):
-        if target_organisms:
-            entries = []
-            queried_tos = set([to.lower() for to in target_organisms])
-            for instance in Experiment.objects.all():
-                instance_tos = set([
-                    to.text.lower()
-                    for to in instance.target_organism.all()
-                ])
-                if queried_tos & instance_tos:
-                    entries.append(instance.pk)
-
-            entries = Experiment.objects.filter(pk__in=entries)
-            return entries
-        return None
-
-    def search_by_contributors(self, model, contributors):
-        if contributors:
-            selected = set()
-            entries = model.objects.none()
-            model_contributors_ls = [
-                (m.pk, m.format_using_full_name('editors', string=True))
-                for m in model.objects.all()
-            ]
-            model_contributors_ls += [
-                (m.pk, m.format_using_username('editors', string=True))
-                for m in model.objects.all()
-            ]
-
-            for c in contributors:
-                for pk, model_contributors in model_contributors_ls:
-                    if model_contributors.lower().find(c.lower()) > -1:
-                        selected.add(pk)
-
-            entries |= model.objects.all().filter(pk__in=selected)
             return entries
         return None
 
@@ -210,17 +141,11 @@ class SearchForm(forms.Form):
             keywords = self.cleaned_data.get(
                 "keywords", None
             ) or search_all
-            authors = self.cleaned_data.get(
-                "contributors", None
-            ) or search_all
             urns = self.cleaned_data.get(
                 "urns", None
             ) or search_all
             metadata_tags = self.cleaned_data.get(
                 "metadata", None
-            ) or search_all
-            targets = self.cleaned_data.get(
-                "targets", None
             ) or search_all
             doi_ids = self.cleaned_data.get(
                 "doi_ids", None
@@ -231,29 +156,19 @@ class SearchForm(forms.Form):
             pmid_ids = self.cleaned_data.get(
                 "pubmed_ids", None
             ) or search_all
-            target_organisms = self.cleaned_data.get(
-                "target_organisms", None
-            ) or search_all
 
             keyword_hits = self.search_by_keyword(model, keywords)
             urns_hits = self.search_by_urn(model, urns)
             metadata_hits = self.search_by_metadata(model, metadata_tags)
-            author_hits = self.search_by_contributors(model, authors)
 
             if model == Experiment:
-                targets_hits = self.search_by_target(model, targets)
                 doi_ids_hits = self.search_by_external_identifier(doi_ids, 'doi_ids')
                 sra_ids_hits = self.search_by_external_identifier(sra_ids, 'sra_ids')
                 pmid_ids_hits = self.search_by_external_identifier(pmid_ids, 'pubmed_ids')
-                target_organism_hits = self.search_by_target_organism(
-                    target_organisms
-                )
             else:
                 doi_ids_hits = None
                 sra_ids_hits = None
                 pmid_ids_hits = None
-                target_organism_hits = None
-                targets_hits = None
 
             if union_search:
                 instances = model.objects.none()
@@ -266,23 +181,11 @@ class SearchForm(forms.Form):
                 else:
                     instances |= keyword_hits
 
-            if author_hits is not None:
-                if not union_search:
-                    instances &= author_hits
-                else:
-                    instances |= author_hits
-
             if urns_hits is not None:
                 if not union_search:
                     instances &= urns_hits
                 else:
                     instances |= urns_hits
-
-            if targets_hits is not None:
-                if not union_search:
-                    instances &= targets_hits
-                else:
-                    instances |= targets_hits
 
             if metadata_hits is not None:
                 if not union_search:
@@ -307,12 +210,6 @@ class SearchForm(forms.Form):
                     instances &= pmid_ids_hits
                 else:
                     instances |= pmid_ids_hits
-
-            if target_organism_hits is not None:
-                if not union_search:
-                    instances &= target_organism_hits
-                else:
-                    instances |= target_organism_hits
 
             return instances
 
