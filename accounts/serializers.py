be88264115d1cbea9ba430380537928edff169a7
Daniel Esposito <danielce90@gmail.com>
2018-04-25 07:45:02 +1000

    Removed contributor_ prefix

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,19 +10,18 @@ class UserSerializer(serializers.ModelSerializer):
     Serializers a :class:`User` instance
     """
     display_name = serializers.CharField(source='profile.get_display_name')
-    contributor_experimentsets = serializers.StringRelatedField(
+    experimentsets = serializers.StringRelatedField(
         source='profile.public_contributor_experimentsets', many=True)
-    contributor_experiments = serializers.StringRelatedField(
+    experiments = serializers.StringRelatedField(
         source='profile.public_contributor_experiments', many=True)
-    contributor_scoresets = serializers.StringRelatedField(
+    scoresets = serializers.StringRelatedField(
         source='profile.public_contributor_scoresets', many=True)
 
     class Meta:
         model = User
         fields = (
             'username', 'last_name', 'first_name', 'display_name',
-            'contributor_experimentsets', 'contributor_experiments',
-            'contributor_scoresets'
+            'experimentsets', 'experiments', 'scoresets'
         )
         read_only_fields = fields
         lookup_field = 'username'
