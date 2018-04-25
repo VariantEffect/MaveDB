5d435b0e870116dfad09fcb59b90c1232a296e23
Daniel Esposito <danielce90@gmail.com>
2018-04-25 21:55:25 +1000

    Added function format_delta to generate a 'last modified on _' string

--- core/utilities/__init__.py
+++ core/utilities/__init__.py
@@ -1,5 +1,31 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 import dataset.constants as constants
+
 
 def is_null(value):
     """Returns True if a stripped/lowercase value in in `nan_col_values`."""
     return str(value).strip().lower() in constants.nan_col_values
+
+
+def format_delta(ta, tb=None):
+    if tb is None:
+        tb = datetime.now()
+
+    t_diff = relativedelta(tb, ta)
+    if t_diff.years:
+        units = "years"
+    elif t_diff.months:
+        units = "months"
+    elif t_diff.weeks:
+        units = "weeks"
+    else:
+        units = "days"
+
+    delta = getattr(t_diff, units)
+    if delta == 0:
+        return "today"
+    if delta == 1:
+        units = units[:-1]
+        return "{} {} ago".format(delta, units)
