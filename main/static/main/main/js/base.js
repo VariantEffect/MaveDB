3bcc902440475c6739ca8e8d5be286c6080a2991
daniel <danielce90@gmail.com>
2018-04-26 17:58:58 +1000

    Fixed bugs in UI and javascript

--- main/static/main/main/js/base.js
+++ main/static/main/main/js/base.js
@@ -4,119 +4,102 @@ jQuery, $;
 
 
 $("form#data").submit(function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    console.log(formData);
+  e.preventDefault();
+  var formData = new FormData(this);
+  console.log(formData);
 });
 
 
 // ORCID button in base.html
 function openORCID() {
-    var baseUrl = window.location.origin;
-    var loginPath = baseUrl + "/accounts/login/";
-    window.location.assign(loginPath);
-    return false;
+  var baseUrl = window.location.origin;
+  var loginPath = baseUrl + "/accounts/login/";
+  window.location.assign(loginPath);
+  return false;
 }
 
 function cancelSubmission() {
-    var baseUrl = window.location.origin;
-    var profileUrl = baseUrl + "/profile/";
-    window.location.assign(profileUrl);
-    return false;
+  var baseUrl = window.location.origin;
+  var profileUrl = baseUrl + "/profile/";
+  window.location.assign(profileUrl);
+  return false;
 }
 
 $("document").ready(function() {
-    // Initialise select2
-    $(".select2").select2();
-    $(".select2-token-select").select2({
-        tags: true,
-        tokenSeparators: [","],
-    });
+  // Initialise select2
+  $(".select2").select2();
+  $(".select2-token-select").select2({
+    tags: true,
+    tokenSeparators: [","],
+  });
 
-    // Re-add any external_accession, keywords or target organism
-    // back to failed form submission
-    repopulateSelect("#id_keywords", "#keywords-to-add");
-    repopulateSelect("#id_sra_ids", "#sra-identifiers-to-add");
-    repopulateSelect("#id_doi_ids", "#doi-identifiers-to-add");
-    repopulateSelect("#id_pubmed_ids", "#pubmed-identifiers-to-add");
-    repopulateSelect("#id_target_organism", "#target-organisms-to-add");
+  // Re-add any external_accession, keywords or target organism
+  // back to failed form submission
+  repopulateSelect("#id_keywords", "#keywords-to-add");
+  repopulateSelect("#id_sra_ids", "#sra-identifiers-to-add");
+  repopulateSelect("#id_doi_ids", "#doi-identifiers-to-add");
+  repopulateSelect("#id_pubmed_ids", "#pubmed-identifiers-to-add");
+  repopulateSelect("#id_uniprot_id", "#uniprot-identifier-to-add");
+  repopulateSelect("#id_ensembl_id", "#ensembl-identifier-to-add");
+  repopulateSelect("#id_refseq_id", "#refseq-identifier-to-add");
 });
 
 
 // Ajax for markdown field preview
 $("#preview-abstract").click(function (e) {
-    // First get whatever is in the form and send an ajax request
-    // to convert it to markdown.
-    var abstract = $("#id_abstract_text").val();
-    $.ajax({
-        url: window.location.pathname,
-        type: "GET",
-        data: { "abstractText": abstract, "markdown": true},
-        dataType: "json",
-        success: function (data) {
-            abstract = data.abstractText;
-            $("#abstract-markdown-modal .modal-body")
-                .text("")
-                .append(abstract);
-        },
-        error : function(xhr, errmsg, err) {
-            console.log(xhr.status + ": " + xhr.responseText);
-        }
-    });
-    return true;
+  // First get whatever is in the form and send an ajax request
+  // to convert it to markdown.
+  var abstract = $("#id_abstract_text").val();
+  $.ajax({
+    url: window.location.pathname,
+    type: "GET",
+    data: { "abstractText": abstract, "markdown": true},
+    dataType: "json",
+    success: function (data) {
+      abstract = data.abstractText;
+      $("#abstract-markdown-modal .modal-body")
+        .text("")
+        .append(abstract);
+    },
+    error : function(xhr, errmsg, err) {
+      console.log(xhr.status + ": " + xhr.responseText);
+    }
+  });
+  return true;
 });
 
 $("#preview-method").click(function (e) {
-    // First get whatever is in the form and send an ajax request
-    // to convert it to markdown.
-    var method = $("#id_method_text").val();
-    $.ajax({
-        url: window.location.pathname,
-        type: "GET",
-        data: { "methodText": method, "markdown": true },
-        dataType: "json",
-        success: function (data) {
-            method = data.methodText;
-            $("#method-markdown-modal .modal-body")
-                .text("")
-                .append(method);
-        },
-        error : function(xhr,errmsg,err) {
-            console.log(xhr.status + ": " + xhr.responseText);
-        }
-    });
-    return true;
-});
-
-// Re-add any external_accession, keywords or target organism
-// back to failed form submission
-function repopulateSelect(selectId, listId) {
-    var ls = $(listId).text();
-    if (ls !== undefined) {
-        ls = ls.trim().split(",");
-        $.each(ls, function (index, value) {
-            if (value !== "") {
-                $(selectId).append($("<option/>", {
-                    value: value,
-                    text: value,
-                    selected: true
-                }));
-            }
-        });
+  // First get whatever is in the form and send an ajax request
+  // to convert it to markdown.
+  var method = $("#id_method_text").val();
+  $.ajax({
+    url: window.location.pathname,
+    type: "GET",
+    data: { "methodText": method, "markdown": true },
+    dataType: "json",
+    success: function (data) {
+      method = data.methodText;
+      $("#method-markdown-modal .modal-body")
+        .text("")
+        .append(method);
+    },
+    error : function(xhr,errmsg,err) {
+      console.log(xhr.status + ": " + xhr.responseText);
     }
-}
-
+  });
+  return true;
+});
 
 // Check Publish is ok with user
 $("#publish").click(function (event) {
-    var saidYes = confirm(
-        "WARNING! Proceeding will freeze your upload and limit which fields " +
-        "can be edited. If this score set is part of a private experiment, " +
-        "this experiment will also be published and frozen. " +
-        "Please make sure you have read the documentation before proceeding." +
-        " This action cannot be undone. Would you like to proceed?"
-    );
-    return saidYes;
+  var saidYes = confirm(
+    "WARNING! Proceeding will freeze your upload and limit which fields " +
+    "can be edited. If this score set is part of a private experiment, " +
+    "this experiment will also be published and frozen. " +
+    "Please make sure you have read the documentation before proceeding." +
+    " This action cannot be undone. Would you like to proceed?"
+  );
+  return saidYes;
 });
 
 
@@ -125,150 +108,208 @@ $("#publish").click(function (event) {
 // `user` is a global defined in base.html using Django"s templating system.
 
 function askConfirmation() {
-    return confirm(
-        "This assignment will remove you as an administartor. If you " +
-        "continue, you will no longer be able to access this page. " +
-        "Are you sure?"
-    );
+  return confirm(
+    "This assignment will remove you as an administartor. If you " +
+    "continue, you will no longer be able to access this page. " +
+    "Are you sure?"
+  );
 }
 
 function validateAdminSubmit(e) {
-    var aSelected = $("#admin-form > div > select option:selected");
-    var aSelectedPks = aSelected.map(function() {
-        return parseInt(this.value);
-    });
+  var aSelected = $("#admin-form > div > select option:selected");
+  var aSelectedPks = aSelected.map(function() {
+    return parseInt(this.value);
+  });
 
-    var willRemoveSelfAsAdmin = aSelectedPks.index(user) < 0;
-    if(willRemoveSelfAsAdmin && !ignore) {
-        var submit = askConfirmation();
-        if(submit) {
-            return $("#admin-form").submit();
-        }
-        return false;
+  var willRemoveSelfAsAdmin = aSelectedPks.index(user) < 0;
+  if(willRemoveSelfAsAdmin && !ignore) {
+    var submit = askConfirmation();
+    if(submit) {
+      return $("#admin-form").submit();
     }
-    return $("#admin-form").submit();
+    return false;
   }
+  return $("#admin-form").submit();
+}
 
 function validateEditorSubmit(e) {
-    var eSelected = $("#editor-form > div > select option:selected");
-    var eSelectedPks = eSelected.map(function() {
-        return parseInt(this.value);
-    });
+  var eSelected = $("#editor-form > div > select option:selected");
+  var eSelectedPks = eSelected.map(function() {
+    return parseInt(this.value);
+  });
 
-    var willRemoveSelfAsAdmin = eSelectedPks.index(user) >= 0;
-    if(willRemoveSelfAsAdmin && !ignore) {
-        var submit = askConfirmation();
-        if(submit) {
-            return $("#editor-form").submit();
-        }
-        return false;
+  var willRemoveSelfAsAdmin = eSelectedPks.index(user) >= 0;
+  if(willRemoveSelfAsAdmin && !ignore) {
+    var submit = askConfirmation();
+    if(submit) {
+      return $("#editor-form").submit();
     }
-    return $("#editor-form").submit();
+    return false;
+  }
+  return $("#editor-form").submit();
 }
 
 function validateViewerSubmit(e) {
-    var vSelected = $("#viewer-form > div > select option:selected");
-    var vSelectedPks = vSelected.map(function() {
-        return parseInt(this.value);
-    });
+  var vSelected = $("#viewer-form > div > select option:selected");
+  var vSelectedPks = vSelected.map(function() {
+    return parseInt(this.value);
+  });
 
-    var willRemoveSelfAsAdmin = vSelectedPks.index(user) >= 0;
-    if(willRemoveSelfAsAdmin && !ignore) {
-        var submit = askConfirmation();
-        if(submit) {
-            return $("#viewer-form").submit();
-        }
-        return false;
+  var willRemoveSelfAsAdmin = vSelectedPks.index(user) >= 0;
+  if(willRemoveSelfAsAdmin && !ignore) {
+    var submit = askConfirmation();
+    if(submit) {
+      return $("#viewer-form").submit();
     }
-    return $("#viewer-form").submit();
+    return false;
+  }
+  return $("#viewer-form").submit();
 }
 
 
-// Dynaimic target selection
+// Dynaimic form selection
 // ------------------------------------------------------------------------ //
-$("#id_target").on("change", function() {
-    // First get whatever is in the form and send an ajax request
-    // to convert it to markdown.
-    var id = this.value;
-    if (parseInt(id)) {
-        $.ajax({
-            url: window.location.pathname,
-            type: "GET",
-            data: {"targetId": id},
-            dataType: "json",
-            success: function (data) {
-                console.log(data);
-                var targetName = data.targetName;
-                var wildTypeSequence = data.wildTypeSequence;
-                var referenceGenome = data.referenceGenome;
-                var isPrimary = data.isPrimary;
-                var intervalStart = data.intervalStart;
-                var intervalEnd = data.intervalEnd;
-                var chromosome = data.chromosome;
-                var strand = data.strand;
-
-                if (targetName) {
-                    $("#id_name").val(targetName);
-                } else {
-                    $("#id_name").val("");
-                }
-                if (wildTypeSequence) {
-                    $("#id_wt_sequence").val(wildTypeSequence);
-                } else {
-                    $("#id_wt_sequence").val("");
-                }
-                if (referenceGenome) {
-                    $("#id_genome").val(referenceGenome);
-                } else {
-                    $("#id_genome").val("");
-                }
-                if (isPrimary) {
-                    $("#id_is_primary").prop('checked', isPrimary);
-                } else {
-                    $("#id_is_primary").prop('checked', false);
-                }
-                if (intervalStart) {
-                    $("#id_start").val(intervalStart);
-                } else {
-                    $("#id_start").val("");
-                }
-                if (intervalEnd) {
-                    $("#id_end").val(intervalEnd);
-                } else {
-                    $("#id_end").val("");
-                }
-                if (chromosome) {
-                    $("#id_chromosome").val(chromosome);
-                } else {
-                    $("#id_chromosome").val("");
-                }
-                if (strand) {
-                    $("#id_strand").val(strand);
-                } else {
-                    $("#id_strand").val('F');
-                }
-            },
-            error: function (xhr, errmsg, err) {
-                console.log(xhr.status + ": " + xhr.responseText);
-            }
-        });
-        return true;
+// Re-add any external_accession, keywords or target organism
+// back to failed form submission
+function repopulateSelect(selectId, listId) {
+  var ls = listId;
+  if (ls !== undefined) {
+    if (typeof listId === "string") {
+      ls = $(listId).text();
+      ls = ls.trim().split(",");
     }
-    return false;
+    $.each(ls, function (index, value) {
+      if (value !== "") {
+        $(selectId).append($("<option/>", {
+          value: value,
+          text: value,
+          selected: true
+        }));
+      }
+    });
+  }
+}
+
+$("#id_experiment").on("change", function() {
+  var id = this.value;
+  var replaces_selector = "#id_replaces";
+  var options = $(replaces_selector).children();
+  if (parseInt(id)) {
+    $.ajax({
+      url: window.location.pathname,
+      type: "GET",
+      data: {"experiment": id},
+      dataType: "json",
+
+      success: function (data) {
+        console.log(data);
+        $.each(options, function (index, option) {
+          if (option.value !== "") {
+            $(option).remove();
+          } else {
+            $(option).select();
+          }
+        });
+        $.each(data.scoresets, function (index, tuple) {
+          if (tuple[0] !== "" && tuple[1] !== "") {
+            $(replaces_selector).append($("<option/>", {
+              value: tuple[0],
+              text: tuple[1]
+            }));
+          }
+        });
+      },
+      error: function (xhr, errmsg, err) {
+        console.log(xhr.status + ": " + errmsg);
+      }
+    })
+  }
+});
+
+
+$("#id_target").on("change", function() {
+  // First get whatever is in the form and send an ajax request
+  // to convert it to markdown.
+  var id = this.value;
+  if (parseInt(id)) {
+    $.ajax({
+      url: window.location.pathname,
+      type: "GET",
+      data: {"targetId": id},
+      dataType: "json",
+      success: function (data) {
+        console.log(data);
+        var targetName = data.name;
+        var wildTypeSequence = data.wt_sequence.sequence;
+        var referenceGenome = data.referenceGenome;
+        var isPrimary = data.isPrimary;
+        var intervalStart = data.intervalStart;
+        var intervalEnd = data.intervalEnd;
+        var chromosome = data.chromosome;
+        var strand = data.strand;
+
+        if (targetName) {
+          $("#id_name").val(targetName);
+        } else {
+          $("#id_name").val("");
+        }
+        if (wildTypeSequence) {
+          $("#id_wt_sequence").val(wildTypeSequence);
+        } else {
+          $("#id_wt_sequence").val("");
+        }
+        if (referenceGenome) {
+          $("#id_genome").val(referenceGenome);
+        } else {
+          $("#id_genome").val("");
+        }
+        if (isPrimary) {
+          $("#id_is_primary").prop('checked', isPrimary);
+        } else {
+          $("#id_is_primary").prop('checked', false);
+        }
+        if (intervalStart) {
+          $("#id_start").val(intervalStart);
+        } else {
+          $("#id_start").val("");
+        }
+        if (intervalEnd) {
+          $("#id_end").val(intervalEnd);
+        } else {
+          $("#id_end").val("");
+        }
+        if (chromosome) {
+          $("#id_chromosome").val(chromosome);
+        } else {
+          $("#id_chromosome").val("");
+        }
+        if (strand) {
+          $("#id_strand").val(strand);
+        } else {
+          $("#id_strand").val('F');
+        }
+      },
+      error: function (xhr, errmsg, err) {
+        console.log(xhr.status + ": " + xhr.responseText);
+      }
+    });
+    return true;
+  }
+  return false;
 });
 
 
 function ensureAtLeastOne() {
-    var num = parseInt(document.getElementById("id_form-TOTAL_FORMS").value);
-    if (num <= 0 ) {
-        return $("#add-interval").trigger("click");
-    }
-    return false;
+  var num = parseInt(document.getElementById("id_form-TOTAL_FORMS").value);
+  if (num <= 0 ) {
+    return $("#add-interval").trigger("click");
+  }
+  return false;
 }
 
 $("#add-interval").click(function(e) {
-    e.preventDefault();
-    $(".add-interval-link").click();
+  e.preventDefault();
+  $(".add-interval-link").click();
 });
 
 
@@ -299,8 +340,8 @@ function sortTable(id, n) {
       x_val = parseFloat(x.innerHTML.toLowerCase());
       y_val = parseFloat(y.innerHTML.toLowerCase());
       if (isNaN(x_val) || isNaN(y_val)) {
-          x_val = x.innerHTML.toLowerCase();
-          y_val = y.innerHTML.toLowerCase();
+        x_val = x.innerHTML.toLowerCase();
+        y_val = y.innerHTML.toLowerCase();
       }
       if (dir === "asc") {
         if (x_val > y_val) {
@@ -335,5 +376,5 @@ function sortTable(id, n) {
 }
 
 function showOptions() {
-    $( ".fade-option" ).toggle("fade");
+  $( ".fade-option" ).toggle("fade");
 }
