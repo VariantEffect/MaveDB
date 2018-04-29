a82487a6ffd924cfd90d71b1117a917a355b18c4
Daniel Esposito <danielce90@gmail.com>
2018-04-30 00:17:19 +1000

    Renamed a few things

--- dataset/constants.py
+++ dataset/constants.py
@@ -16,11 +16,11 @@ nan_col_values = {
 hgvs_column = "hgvs"
 score_columns = "score_columns"
 count_columns = "count_columns"
-metadata_columns = "metadata_columns"
+meta_columns = "meta_columns"
 variant_score_data = 'score_data'
 variant_count_data = 'count_data'
+variant_meta_data = 'meta_data'
 required_score_column = 'score'
-variant_metadata = 'metadata'
 
 experimentset_url_pattern = MAVEDB_EXPERIMENTSET_URN_PATTERN[1:-1]
 experiment_url_pattern = MAVEDB_EXPERIMENT_URN_PATTERN[1:-1]
@@ -33,19 +33,19 @@ any_url_pattern = '|'.join([
 valid_dataset_columns = [
     score_columns,
     count_columns,
-    metadata_columns
+    meta_columns
 ]
 valid_variant_columns = [
     variant_score_data,
     variant_count_data,
-    variant_metadata
+    variant_meta_data
 ]
 
 variant_to_scoreset_column = {
     variant_score_data: score_columns,
     variant_count_data: count_columns,
-    variant_metadata: metadata_columns
+    variant_meta_data: meta_columns
 }
 scoreset_to_variant_column = {
-    v:k for k, v in variant_to_scoreset_column.items()
+    v: k for k, v in variant_to_scoreset_column.items()
 }
