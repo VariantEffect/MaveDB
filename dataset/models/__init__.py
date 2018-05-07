3cc1ce87026455a60bc065e7a18ac4f3f2a53799
daniel <danielce90@gmail.com>
2018-05-07 19:52:44 +1000

    Moved get_by_urn to urn app

--- dataset/models/__init__.py
+++ dataset/models/__init__.py
@@ -1,20 +1,7 @@
-from django.core.exceptions import ObjectDoesNotExist
-
 __all__ = [
     "experiment",
     'experimentset',
     'scoreset',
-    'get_model_by_urn',
 ]
 
 
-def get_model_by_urn(urn):
-    from variant.models import Variant
-    from .scoreset import ScoreSet
-    from .experiment import Experiment
-    from .experimentset import ExperimentSet
-    
-    for model in [ScoreSet, Experiment, ExperimentSet, Variant]:
-        if model.objects.filter(urn=urn).exists():
-            return model.objects.get(urn=urn)
-    raise ObjectDoesNotExist("No model found with urn {}.".format(urn))
