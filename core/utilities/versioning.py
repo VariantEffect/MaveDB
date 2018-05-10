6514b30d6cad58a7712ef06c5282cfb5e98eb5a7
daniel <danielce90@gmail.com>
2018-05-10 18:43:01 +1000

    reversion updates

--- core/utilities/versioning.py
+++ core/utilities/versioning.py
@@ -1,9 +1,12 @@
-from datetime import datetime
 import reversion
 from reversion.models import Version
 
+from django.db import transaction
+from django.db.models import Manager
+from django.utils import timezone
 
-def track_changes(user, instance):
+
+def track_changes(instance, user=None):
     comments = []
     klass = instance.__class__.__name__
     versions = Version.objects.get_for_object(instance)
@@ -15,6 +18,8 @@ def track_changes(user, instance):
         for field in instance.tracked_fields():
             p_field = prev_version.field_dict.get(field)
             n_field = getattr(instance, field)
+            if isinstance(n_field, Manager):
+                n_field = [i.id for i in n_field.all()]
             if isinstance(p_field, (list, set, tuple)):
                 p_field = sorted(p_field)
             if isinstance(n_field, (list, set, tuple)):
@@ -29,5 +34,41 @@ def track_changes(user, instance):
         with reversion.create_revision():
             instance.save()
             reversion.set_user(user)
-            reversion.set_date_created(datetime.today())
+            reversion.set_date_created(timezone.now())
             reversion.set_comment(', '.join(comments))
+
+
+@transaction.atomic
+def revert(instance):
+    """
+    Rollsback fields to the last version
+
+    Using previous_version.revision.revert() will throw a RevertError
+    """
+    versions = Version.objects.get_for_object(instance)
+    if versions.count():
+        previous_version = versions[0]
+        for field in instance.tracked_fields():
+            previous_field_value = previous_version.field_dict.get(field)
+            current_field_value = getattr(instance, field)
+            if isinstance(previous_field_value, (set, list, tuple)):
+                current_field_value.clear()
+                for pk in previous_field_value:
+                    current_field_value.add(pk)
+            else:
+                setattr(instance, field, previous_field_value)
+
+        with reversion.create_revision():
+            reversion.set_date_created(timezone.now())
+            reversion.set_comment(
+                "Reverted to revision Version instance with id {} "
+                "created on {} by user {}.".format(
+                    previous_version.revision.id,
+                    previous_version.revision.date_created,
+                    previous_version.revision.user
+                )
+            )
+            instance.save()
+
+    instance.refresh_from_db()
+    return instance
