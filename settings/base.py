e310ae3196be2216cddb6a7e4ee52512b16ef1f4
Daniel Esposito <danielce90@gmail.com>
2018-05-23 16:11:10 +1000

    celery debug

--- settings/base.py
+++ settings/base.py
@@ -6,6 +6,8 @@ from datetime import timedelta
 
 from django.core.exceptions import ImproperlyConfigured
 
+DEBUG = True
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SETTINGS_DIR = BASE_DIR + '/settings/'
