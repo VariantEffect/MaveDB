b29f9523f6f526c29e85c46734149d368a62c926
daniaki <danielce90@gmail.com>
2018-06-07 21:01:37 +1000

    baseurl defaults to http if not request supplied

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,22 +1,24 @@
 """
 Add any functions here which you would like for use in templates.
 """
-from main.models import SiteInformation
+from django.conf import settings
+
 
 def baseurl(request):
     """
-    Return a BASE_URL template context for the current request.
+    Return a base_url template context for the current request.
     """
-    if request.is_secure():
-        scheme = 'https://'
-    else:
+    if request is None:
         scheme = 'http://'
-
-    return {
-        'BASE_URL': scheme + request.get_host(),
-    }
+    else:
+        if request.is_secure():
+            scheme = 'https://'
+        else:
+            scheme = 'http://'
+    return {'base_url': scheme + settings.HOST_NAME}
 
 
 def site_information(request):
     """Adds the SiteInformation singleton to all requests."""
+    from .models import SiteInformation
     return {'site_information': SiteInformation.get_instance()}
