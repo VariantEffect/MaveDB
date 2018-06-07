7f15889b6c86461585cfe63d4652356cbd90bcd8
daniaki <danielce90@gmail.com>
2018-06-07 21:06:58 +1000

    PEP8 formatting

--- dataset/apps.py
+++ dataset/apps.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.management import create_permissions
 from django.apps import apps
 
+
 class DatasetConfig(apps.AppConfig):
     name = 'dataset'
+
 
 create_permissions(apps.get_app_config('dataset'))
