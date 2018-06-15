214f34733eacc082a7a6c1e44551eea1d1a9339a
daniel <danielce90@gmail.com>
2018-06-15 13:51:16 +1000

    Using get_instance instead of objects.first

--- main/management/commands/updatesiteinfo.py
+++ main/management/commands/updatesiteinfo.py
@@ -16,11 +16,9 @@ class Command(BaseCommand):
         path = os.path.join(settings.MAIN_DIR, 'site_info.json')
         with open(path, 'rt') as fp:
             info = json.load(fp)
-            instance = SiteInformation.objects.first()
-            if not instance:
-                sys.stdout.write("Creating new instance.\n")
-                instance = SiteInformation()
+            instance = SiteInformation.get_instance()
             for field, value in info.items():
                 sys.stdout.write("Setting field '{}'.\n".format(field))
                 setattr(instance, field, value)
             instance.save()
+        sys.stdout.write("Updated 'data/site_info.json'.\n".format(field))
