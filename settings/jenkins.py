02688efef39e99ac451cedc1a42ca13e8dd907bd
daniel <danielce90@gmail.com>
2018-06-28 15:45:47 +1000

    Jenkins settings which is staging with a local logging directory

--- settings/jenkins.py
+++ settings/jenkins.py
@@ -1,79 +1,14 @@
 # settings/staging.py
 from .staging import *
 
-# Set up logging
-# Set up logging
+import logging
+
+# Disable logging
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '[%(levelname)s] %(asctime)s %(module)s %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': './info.log',
-            'formatter': 'verbose'
-        },
-        'celery': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': './celery.log',
-            'formatter': 'verbose'
-        },
-        'core.tasks': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': './celery_core_tasks.log',
-            'formatter': 'verbose'
-        },
-        'accounts.tasks': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': './celery_accounts_tasks.log',
-            'formatter': 'verbose'
-        },
-        'dataset.tasks': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': './celery_dataset_tasks.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False
-        },
-        'celery': {
-            'handlers': ['celery'],
-            'level': 'INFO',
-            'propagate': True
-        },
-        'core.tasks': {
-            'handlers': ['core.tasks'],
-            'level': 'INFO',
-            'propagate': True
-        },
-        'accounts.tasks': {
-            'handlers': ['accounts.tasks'],
-            'level': 'INFO',
-            'propagate': True
-        },
-        'dataset.tasks': {
-            'handlers': ['dataset.tasks'],
-            'level': 'INFO',
-            'propagate': True
-        },
-    },
+    'disable_existing_loggers': True,
 }
+logging.disable(logging.CRITICAL)
 
 # ------ CELERY CONFIG ------------------- #
 # Celery needs these in each settings file
