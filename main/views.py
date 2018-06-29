2d4bc1aa2942ad096a6a43ce0cc47fca449080a9
daniel <danielce90@gmail.com>
2018-06-29 17:02:49 +1000

    Incorrect redirect url with new patterns

--- main/views.py
+++ main/views.py
@@ -80,17 +80,17 @@ def terms_privacy_view(request):
     })
 
 
-def handler404(request, exception=None, template_name='main/404.html'):
-    response = render(
-        request, template_name, context={})
-    response.status_code = 404
-    return response
-
-
 def handler403(request, exception=None, template_name='main/403.html'):
     response = render(
         request, template_name, context={})
     response.status_code = 403
+    return response
+
+
+def handler404(request, exception=None, template_name='main/404.html'):
+    response = render(
+        request, template_name, context={})
+    response.status_code = 404
     return response
 
 
