1fd0e1fa577300688581a07dc5761d16577dc141
daniaki <danielce90@gmail.com>
2018-06-30 20:58:25 +1000

    PEP8 formatting

--- settings/base.py
+++ settings/base.py
@@ -210,7 +210,7 @@ REST_FRAMEWORK = {
 
 # ------ CELERY CONFIG ------------------- #
 # Celery needs these in each settings file
-DATASET_TASK_SOFT_TIME_LIMIT =  10 * 60  # In seconds
+DATASET_TASK_SOFT_TIME_LIMIT = 10 * 60  # In seconds
 broker_url = 'amqp://localhost:5672//'
 accept_content = ('json',)
 result_serializer = 'json'
