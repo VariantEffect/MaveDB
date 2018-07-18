2ac2d9025bbf1f76e172dc8709d2c5df66e7b1a7
daniaki <danielce90@gmail.com>
2018-07-18 21:19:24 +1000

    Publish done through publish_dataset which should now be thread safe

--- dataset/utilities.py
+++ dataset/utilities.py
@@ -1,4 +1,9 @@
+import datetime
+
 from django.db import transaction
+
+from dataset import models
+from variant.models import Variant
 
 from .models.experimentset import ExperimentSet
 from .models.experiment import Experiment
@@ -52,3 +57,49 @@ def delete_scoreset(scoreset):
         variant.delete()
     scoreset.delete()
 
+
+@transaction.atomic(savepoint=True)
+def publish_dataset(dataset, user=None):
+    if not dataset.private or dataset.has_public_urn:
+        return dataset
+
+    if dataset.parent:
+        publish_dataset(dataset.parent, user)
+
+    dataset.refresh_from_db()
+    if isinstance(dataset, models.experimentset.ExperimentSet):
+        counter = models.base.PublicDatasetCounter.objects. \
+            select_for_update(nowait=False).first()
+        dataset = models.experimentset.ExperimentSet.objects.filter(
+            id=dataset.id
+        ).select_for_update(nowait=False).first()
+        dataset = models.experimentset.assign_public_urn(dataset, counter)
+
+    elif isinstance(dataset, models.experiment.Experiment):
+        dataset = models.experiment.Experiment.objects.filter(
+            id=dataset.id
+        ).select_for_update(nowait=False).first()
+        dataset = models.experiment.assign_public_urn(dataset)
+
+    elif isinstance(dataset, models.scoreset.ScoreSet):
+        dataset = models.scoreset.ScoreSet.objects.filter(
+            id=dataset.id
+        ).select_for_update(nowait=False).first()
+        dataset = models.scoreset.assign_public_urn(dataset)
+        urns = Variant.bulk_create_urns(dataset.children.count(), dataset)
+        for urn, child in zip(urns, dataset.children.all()):
+            child.urn = urn
+            child.save()
+
+    else:
+        raise TypeError(
+            "Expected ExperimentSet, Experiment or ScoreSet. Found {}".format(
+                dataset.__class__.__name__
+            )
+        )
+
+    dataset.publish_date = datetime.date.today()
+    dataset.private = False
+    dataset.set_modified_by(user, propagate=False)
+    dataset.save()
+    return dataset
