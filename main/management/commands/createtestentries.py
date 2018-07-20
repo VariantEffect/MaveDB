ef259a6f834198a463e0a415fa76cece39452ec5
daniel <danielce90@gmail.com>
2018-07-20 13:50:38 +1000

    Updated to use new publish method

--- main/management/commands/createtestentries.py
+++ main/management/commands/createtestentries.py
@@ -16,14 +16,10 @@ from genome import factories as genome_factories
 from genome import models  as genome_models
 from variant.factories import VariantFactory
 
-
 User = get_user_model()
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument('clear', nargs='?', type=bool, default=False)
-
     def handle(self, *args, **kwargs):
         password = "1234qwer"
         with transaction.atomic():
@@ -32,12 +28,12 @@ class Command(BaseCommand):
                 user.set_password(password)
                 user.save()
                 instance = factories.ExperimentFactory()
-                for publish in [False, True]:
+                for _ in [False, True]:
                     # Configure the scoreset first.
                     scoreset = factories.ScoreSetFactory(experiment=instance)
                     target = genome_factories.TargetGeneFactory()
                     genomes = genome_models.ReferenceGenome.objects.all()
-                    genome_models.ReferenceMap(
+                    genome_factories.ReferenceMapFactory(
                         target=target,
                         genome=random.choice(genomes),
                         is_primary=True
@@ -45,9 +41,10 @@ class Command(BaseCommand):
                     target.scoreset = scoreset
                     target.save()
                     scoreset.save()
-
+                
                 for scoreset, publish in zip(
                         instance.children.all(), [True, False]):
+                    scoreset.refresh_from_db()
                     UniprotOffset.objects.create(
                         offset=i*3 + 1,
                         target=scoreset.target,
@@ -65,13 +62,22 @@ class Command(BaseCommand):
                     )
 
                     if publish:
-                        utilities.publish_dataset(scoreset)
+                        scoreset = utilities.publish_dataset(scoreset)
 
                     scoreset.set_modified_by(user, propagate=True)
                     scoreset.set_created_by(user, propagate=True)
-                    for i in range(10):
+                    
+                    for _ in range(100):
                         VariantFactory(scoreset=scoreset)
-                    scoreset.save(save_parents=True)
+                    scoreset.save()
+                    
+                    experiment = scoreset.parent
+                    experiment.refresh_from_db()
+                    experiment.save()
+                    
+                    experimentset = experiment.experimentset
+                    experimentset.refresh_from_db()
+                    experimentset.save()
 
                     scoreset.add_administrators(user)
                     scoreset.experiment.add_administrators(user)
