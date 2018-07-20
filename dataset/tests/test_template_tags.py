dcd05513f8615da78e6aaacbb8784a673a77ad7b
daniel <danielce90@gmail.com>
2018-07-20 17:33:34 +1000

    Testing template tags

--- dataset/tests/test_template_tags.py
+++ dataset/tests/test_template_tags.py
@@ -1,3 +1,5 @@
+import json
+
 from django.test import TestCase
 
 from accounts.factories import UserFactory
@@ -31,6 +33,16 @@ class TestDisplayTargetsTag(TestCase):
         result = dataset_tags.display_targets(exp, user)
         self.assertIn(exp.scoresets.first().get_target().get_name(), result)
 
+    def test_formats_for_javascript(self):
+        exp = ExperimentWithScoresetFactory()
+        user = UserFactory()
+        exp.scoresets.first().add_viewers(user)
+        result = dataset_tags.display_targets(exp, user, javascript=True)
+        self.assertEqual(
+            result,
+            json.dumps(sorted([exp.scoresets.first().get_target().get_name()]))
+        )
+
 
 class TestDisplaySpeciesTag(TestCase):
     def test_shows_species_from_public_scoresets(self):
@@ -61,6 +73,20 @@ class TestDisplaySpeciesTag(TestCase):
             list(exp.scoresets.first().get_display_target_organisms())[0],
             result
         )
+        
+    def test_formats_for_javascript(self):
+        exp = ExperimentWithScoresetFactory()
+        user = UserFactory()
+        exp.scoresets.first().add_viewers(user)
+        result = dataset_tags.display_species(exp, user, javascript=True)
+        self.assertEqual(
+            result,
+            json.dumps(
+                sorted(list(
+                    exp.scoresets.first().get_display_target_organisms())
+                ))
+        )
+
 
 
 class TestVisibleChildren(TestCase):
@@ -101,3 +127,33 @@ class TestParentReferences(TestCase):
         pmids = dataset_tags.parent_references(instance.children.first())
         self.assertEqual(len(pmids), 1)
         self.assertEqual(pmids[0], pm1)
+
+
+class TestUrnNameFormatTag(TestCase):
+    def test_adds_private_if_user_is_contributor(self):
+        user = UserFactory()
+        instance = ExperimentWithScoresetFactory()
+        instance.add_administrators(user)
+        self.assertIn(
+            '[Private]',
+            dataset_tags.format_urn_name_for_user(instance, user)
+        )
+        
+    def test_does_not_add_private_if_user_is_not_contributor(self):
+        user = UserFactory()
+        instance = ExperimentWithScoresetFactory()
+        self.assertNotIn(
+            '[Private]',
+            dataset_tags.format_urn_name_for_user(instance, user)
+        )
+        
+    def test_does_not_add_private_public_instance(self):
+        user = UserFactory()
+        instance = ExperimentWithScoresetFactory()
+        instance.add_administrators(user)
+        publish_dataset(instance)
+        self.assertIn(
+            '[Private]',
+            dataset_tags.format_urn_name_for_user(instance, user)
+        )
+        
