0d88e0fdfd64ccb4baa18cc61b59aeefce51940e
daniaki <danielce90@gmail.com>
2018-07-30 21:07:58 +1000

    Filter field for comma separated lists

---
+++ core/filters.py
@@ -0,0 +1,25 @@
+import csv
+
+from django.core.exceptions import ImproperlyConfigured
+
+from django_filters import filters, FilterSet
+
+
+class MultiCharFilter(filters.CharFilter):
+    sep = None
+
+    def filter(self, qs, value):
+        if not self.sep:
+            raise ImproperlyConfigured("MultiCharFilter is abstract.")
+        if value:
+            value = list(csv.reader([value], delimiter=self.sep))[0]
+        if not isinstance(value, list):
+            value = [value]
+        result = qs.none()
+        for v in value:
+            result |= super().filter(qs, v)
+        return result
+
+
+class CSVCharFilter(MultiCharFilter):
+    sep = ','
