f2d319ca564e64b0bc9e620a4f6d55e72129967a
daniel <danielce90@gmail.com>
2018-11-26 19:50:30 +1100

    Utility commands

--- main/management/commands/geterror.py
+++ main/management/commands/geterror.py
@@ -1,5 +1,4 @@
 import sys
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from core.models import FailedTask
@@ -8,20 +7,32 @@ from core.models import FailedTask
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('--taskid', type=str, help="Task reference id.",)
+        parser.add_argument(
+            '--taskid', type=str, default=None, help="Task reference id.",)
+        parser.add_argument(
+            '--username', type=str, default=None, help="Submitter's username",)
 
     def handle(self, *args, **kwargs):
-        taskid = kwargs.get('--taskid', None)
+        taskid = kwargs.get('taskid', None)
+        username = kwargs.get('username', None)
         if taskid:
             taskid = str(taskid)
+            tasks = FailedTask.objects.filter(celery_task_id=taskid)
+        elif username:
+            tasks = FailedTask.objects.filter(user__username=username)
         else:
-            raise ValueError("Task ID required.")
+            raise ValueError("Task ID or Username required.")
+        
+        if not tasks.count():
+            sys.stdout.write('No tasks found')
 
-        task = FailedTask.objects.get(celery_task_id=taskid)
-        sys.stdout.write('Name: {}\n'.format(task.name))
-        sys.stdout.write('Full name: {}\n'.format(task.full_name))
-        sys.stdout.write('args: {}\n'.format(task.args))
-        sys.stdout.write('kwargs: {}\n'.format(task.kwargs))
-        sys.stdout.write('Exception class: {}\n'.format(task.exception_class))
-        sys.stdout.write('Exception: {}\n'.format(task.exception_msg))
-        sys.stdout.write('Traceback: {}\n'.format(task.traceback))
+        for task in tasks.all():
+            sys.stdout.write('{}\n'.format('-'* len(task.full_name)))
+            sys.stdout.write('Name: {}\n'.format(task.name))
+            sys.stdout.write('Full name: {}\n'.format(task.full_name))
+            sys.stdout.write('args: {}\n'.format(task.args))
+            sys.stdout.write('kwargs: {}\n'.format(task.kwargs))
+            sys.stdout.write('Exception class: {}\n'.format(task.exception_class))
+            sys.stdout.write('Exception: {}\n'.format(task.exception_msg))
+            sys.stdout.write('Traceback: {}\n'.format(task.traceback))
+            sys.stdout.write('{}\n'.format('-' * len(task.full_name)))
