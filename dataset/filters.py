7f08b52292bcfedb3d6b9c31efc3c577d11f266a
daniel <danielce90@gmail.com>
2019-02-08 19:56:51 +1100

    Added filters for `target_type`

--- dataset/filters.py
+++ dataset/filters.py
@@ -192,6 +192,10 @@ class ExperimentFilter(DatasetModelFilter):
     uniprot = CSVCharFilter(method='filter_by_scoreset')
     ensembl = CSVCharFilter(method='filter_by_scoreset')
     refseq = CSVCharFilter(method='filter_by_scoreset')
+    target_type = CSVCharFilter(
+        field_name="scoresets__target__category",
+        lookup_expr='icontains',
+    )
 
     def filter_by_scoreset(self, queryset, name, value):
         experiments = set()
@@ -257,6 +261,10 @@ class ScoreSetFilter(DatasetModelFilter):
     refseq = CSVCharFilter(
         field_name='target__refseq_id__identifier',
         lookup_expr='iexact'
+    )
+    target_type = CSVCharFilter(
+        field_name="target__category",
+        lookup_expr='icontains',
     )
     
     def filter_licence(self, queryset, name, value):
