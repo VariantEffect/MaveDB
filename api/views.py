308b40dccea4ad354937541ac107cc6decedb3d0
daniel <danielce90@gmail.com>
2019-02-11 12:30:39 +1100

    Added additional GET endpoints for meta and genome models

--- api/views.py
+++ api/views.py
@@ -10,6 +10,12 @@ from django.http import HttpResponse, JsonResponse
 from accounts.models import AUTH_TOKEN_RE, Profile
 from accounts.filters import UserFilter
 
+from metadata import models as meta_models
+from metadata import serializers as meta_serializers
+
+from genome import models as genome_models
+from genome import serializers as genome_serializers
+
 from core.utilities import chunks
 
 from dataset import models, filters, constants
@@ -98,6 +104,7 @@ class DatasetListViewSet(AuthenticatedViewSet):
 
 
 class ExperimentSetViewset(DatasetListViewSet):
+    http_method_names = ('get',)
     serializer_class = ExperimentSetSerializer
     filter_class = filters.ExperimentSetFilterModel
     model_class = models.experimentset.ExperimentSet
@@ -106,6 +113,7 @@ class ExperimentSetViewset(DatasetListViewSet):
 
 
 class ExperimentViewset(DatasetListViewSet):
+    http_method_names = ('get',)
     serializer_class = ExperimentSerializer
     filter_class = filters.ExperimentFilter
     model_class = models.experiment.Experiment
@@ -114,6 +122,7 @@ class ExperimentViewset(DatasetListViewSet):
 
 
 class ScoreSetViewset(DatasetListViewSet):
+    http_method_names = ('get',)
     serializer_class = ScoreSetSerializer
     filter_class = filters.ScoreSetFilter
     model_class = models.scoreset.ScoreSet
@@ -122,7 +131,8 @@ class ScoreSetViewset(DatasetListViewSet):
 
 
 class UserViewset(AuthenticatedViewSet):
-    queryset = User.objects.exclude(username='AnonymousUser')
+    http_method_names = ('get', )
+    queryset = User.objects.all()
     serializer_class = UserSerializer
     filter_class = UserFilter
     lookup_field = 'username'
@@ -306,3 +316,66 @@ def scoreset_metadata(request, urn):
         return instance_or_response
     scoreset = instance_or_response
     return JsonResponse(scoreset.extra_metadata, status=200)
+
+
+# ----- Other API endpoints
+class KeywordViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get', )
+    queryset = meta_models.Keyword.objects.all()
+    serializer_class = meta_serializers.KeywordSerializer
+
+
+class PubmedIdentifierViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get', )
+    queryset = meta_models.PubmedIdentifier.objects.all()
+    serializer_class = meta_serializers.PubmedIdentifierSerializer
+    
+    
+class SraIdentifierViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get', )
+    queryset = meta_models.SraIdentifier.objects.all()
+    serializer_class = meta_serializers.SraIdentifierSerializer
+    
+    
+class DoiIdentifierViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get', )
+    queryset = meta_models.DoiIdentifier.objects.all()
+    serializer_class = meta_serializers.DoiIdentifierSerializer
+    
+    
+class EnsemblIdentifierViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get', )
+    queryset = meta_models.EnsemblIdentifier.objects.all()
+    serializer_class = meta_serializers.EnsemblIdentifierSerializer
+
+
+class RefseqIdentifierViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get',)
+    queryset = meta_models.RefseqIdentifier.objects.all()
+    serializer_class = meta_serializers.RefseqIdentifierSerializer
+    
+    
+class UniprotIdentifierViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get',)
+    queryset = meta_models.UniprotIdentifier.objects.all()
+    serializer_class = meta_serializers.UniprotIdentifierSerializer
+    
+    
+class GenomeIdentifierViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get',)
+    queryset = meta_models.GenomeIdentifier.objects.all()
+    serializer_class = meta_serializers.GenomeIdentifierSerializer
+    
+    
+class TargetGeneViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get',)
+    queryset = genome_models.TargetGene.objects.exclude(
+        scoreset__private=True
+    )
+    serializer_class = genome_serializers.TargetGeneSerializer
+    
+    
+class ReferenceGenomeViewSet(viewsets.ModelViewSet):
+    http_method_names = ('get',)
+    queryset = genome_models.ReferenceGenome.objects.all()
+    serializer_class = genome_serializers.ReferenceGenomeSerializer
