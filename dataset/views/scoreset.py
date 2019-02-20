79c13e15a98926cef506f6308ff63d9dd0cffacf
daniel <danielce90@gmail.com>
2019-02-20 21:41:23 +1100

    only show current version

--- dataset/views/scoreset.py
+++ dataset/views/scoreset.py
@@ -70,26 +70,27 @@ class ScoreSetDetailView(AjaxView, DatasetModelView):
         context["score_columns"] = instance.score_columns
         context["count_columns"] = instance.count_columns
 
-        previous_version = instance.previous_version
-        if previous_version is not None and previous_version.private and \
-                self.request.user not in previous_version.contributors():
-            previous_version = instance.previous_public_version
-        if previous_version and previous_version.urn == instance.urn:
-            previous_version = None
+        previous_version = instance.get_previous_version(self.request.user)
+        
+        # if previous_version is not None and previous_version.private and \
+        #         self.request.user not in previous_version.contributors():
+        #     previous_version = instance.previous_public_version
+        # if previous_version and previous_version.urn == instance.urn:
+        #     previous_version = None
 
-        next_version = instance.next_version
-        if next_version is not None and next_version.private and \
-                self.request.user not in next_version.contributors():
-            next_version = instance.next_public_version
-        if next_version and next_version.urn == instance.urn:
-            next_version = None
+        next_version = instance.get_next_version(self.request.user)
+        # if next_version is not None and next_version.private and \
+        #         self.request.user not in next_version.contributors():
+        #     next_version = instance.next_public_version
+        # if next_version and next_version.urn == instance.urn:
+        #     next_version = None
 
-        current_version = instance.current_version
-        if current_version is not None and current_version.private and \
-                self.request.user not in current_version.contributors():
-            current_version = instance.current_public_version
-        if current_version and current_version.urn == instance.urn:
-            current_version = None
+        current_version = instance.get_current_version(self.request.user)
+        # if current_version is not None and current_version.private and \
+        #         self.request.user not in current_version.contributors():
+        #     current_version = instance.current_public_version
+        # if current_version and current_version.urn == instance.urn:
+        #     current_version = None
         
         keywords = set([kw for kw in instance.keywords.all()])
         keywords = sorted(
