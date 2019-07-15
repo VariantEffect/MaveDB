a95fbb9c6954c39cc356fd1e2942b83cb2e43d40
daniaki <danielce90@gmail.com>
2019-07-15 12:41:19 +1000

    Added export/import resource for Pageview and Visitor models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
+from import_export import resources
+from tracking.models import Pageview, Visitor
+
 from django.contrib import admin
 from reversion.models import Version
 
@@ -7,3 +10,59 @@ from . import models
 admin.site.register(models.FailedTask)
 admin.site.register(Version)
 
+
+# Define import-export CSV resources
+class PageviewResource(resources.ModelResource):
+    class Meta:
+        model = Pageview
+        fields = (
+            "id",
+            "visitor",
+            "visitor__ip_address",
+            "url",
+            "referer",
+            "query_string",
+            "method",
+            "view_time",
+        )
+        export_order = (
+            "id",
+            "visitor",
+            "visitor__ip_address",
+            "url",
+            "referer",
+            "query_string",
+            "method",
+            "view_time",
+        )
+
+
+class VisitorResource(resources.ModelResource):
+    class Meta:
+        model = Visitor
+        fields = (
+            "user",
+            "user__id",
+            "user__username",
+            "session_key",
+            "ip_address",
+            "user_agent",
+            "start_time",
+            "expiry_age",
+            "expiry_time",
+            "time_on_site",
+            "end_time",
+        )
+        export_order = (
+            "user",
+            "user__id",
+            "user__username",
+            "session_key",
+            "ip_address",
+            "user_agent",
+            "start_time",
+            "expiry_age",
+            "expiry_time",
+            "time_on_site",
+            "end_time",
+        )
