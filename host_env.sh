32207424036bf768f6d2e59810bdc4bcfac71929
daniaki <danielce90@gmail.com>
2020-09-10 18:07:06 +1000

    Added extra exports for dev evnironments

--- host_env.sh
+++ host_env.sh
@@ -1,8 +1,12 @@
-export MAVEDB_DB_USER=
-export MAVEDB_DB_PASSWORD=
-export MAVEDB_DB_PORT=
-export MAVEDB_DB_NAME=
+export MAVEDB_DB_USER=mave_admin
+export MAVEDB_DB_PASSWORD=abc123
+export MAVEDB_DB_PORT=5433
+export MAVEDB_DB_NAME=mavedb
+export MAVEDB_BROKER_PORT=5673
 
-export MAVEDB_BROKER_PORT=
+# Production only settings
 export MAVEDB_RELEASE_TAG=
 export MAVEDB_RUN_TESTS=
+
+# Shell
+export DJANGO_SETTINGS_MODULE=settings.development
