3b538e04027b21485a11fbb90e972c0574274556
daniaki <danielce90@gmail.com>
2020-09-28 15:39:38 +1000

    Additional permission checks when publishing a score set to make sure admin on whole parent tree if publishing private for the first time.

--- accounts/utilities.py
+++ accounts/utilities.py
@@ -42,12 +42,12 @@ def delete(urn, request):
                 "Child {child_class}s must be deleted prior "
                 "to deleting this {parent_class}."
             ).format(
-                child_class=instance.children.first().__class__.__name__.replace(
-                    "Set", " Set"
-                ),
+                child_class=instance.children.first()
+                .__class__.__name__.replace("Set", " set")
+                .lower(),
                 parent_class=instance.__class__.__name__.replace(
-                    "Set", " Set"
-                ),
+                    "Set", " set"
+                ).lower(),
             )
             messages.error(request, message)
             return False
@@ -103,32 +103,65 @@ def publish(urn, request):
         - urn exists and is a score set
         - requesting user has permission
         - is not being processed by celery
-        - has variants that are associated
+        - dataset has variants that are associated and is not empty
         - is not in the 'failed' celery state
         - has not been published already
+        - Use has permission to publish the whole parent tree (
+          has manage permissions)
     """
+
     try:
-        instance = get_model_by_urn(urn=urn)
+        instance = get_model_by_urn(urn=urn)  # type: ScoreSet
         if not request.user.has_perm(PermissionTypes.CAN_MANAGE, instance):
-            raise PermissionDenied()
+            raise PermissionDenied(
+                f"You must be an administrator for {instance.urn} to "
+                f"publish it."
+            )
+
+        if not isinstance(instance, ScoreSet):
+            messages.error(request, "Only score sets can be published.")
+            return False
+
+        experiment = instance.experiment
+        experimentset = instance.experiment.experimentset
+        admin_for_exp_set = request.user.has_perm(
+            PermissionTypes.CAN_MANAGE, experimentset
+        )
+        admin_for_exp = request.user.has_perm(
+            PermissionTypes.CAN_MANAGE, experiment
+        )
+        if instance.is_meta_analysis:
+            if experimentset.private and experimentset.is_mixed_meta_analysis:
+                # This shouldn't happen because a meta-analysis can only be
+                # linked to a published score set, meaning the experiment set
+                # will always be public.
+                if not admin_for_exp_set:
+                    raise PermissionDenied(
+                        f"You must be an administrator for experiment set "
+                        f"{experimentset.urn} before being able to "
+                        f"publish {instance.urn}."
+                    )
+        else:
+            if experiment.private and not admin_for_exp:
+                raise PermissionDenied(
+                    f"You must be an administrator for experiment "
+                    f"{experiment.urn} before being able to "
+                    f"publish {instance.urn}."
+                )
+            if experiment.private and not admin_for_exp_set:
+                raise PermissionDenied(
+                    f"You must be an administrator for experiment set "
+                    f"{experimentset.urn} before being able to "
+                    f"publish {instance.urn}."
+                )
     except ObjectDoesNotExist:
         messages.error(
             request,
-            "Could not find {}. It may have been " "deleted.".format(urn),
+            f"Could not find {urn}. It may have been deleted.",
         )
         return False
-    except PermissionDenied:
-        messages.error(
-            request,
-            "You must be an administrator for {} to publish "
-            "it.".format(urn),
-        )
-        return False
-
-    if not isinstance(instance, ScoreSet):
-        messages.error(
-            request, "Only Score Sets can be published.".format(urn)
-        )
+    except PermissionDenied as error:
+        messages.error(request, str(error))
         return False
 
     # Check the processing state
@@ -177,8 +210,8 @@ def publish(urn, request):
         if success:
             messages.success(
                 request,
-                "{} has been queued for publication. Editing has been "
-                "disabled until your submission has been processed. A public urn "
+                "{} has been queued for publication. Editing has been disabled "
+                "until your submission has been processed. A public urn "
                 "will be assigned upon successful completion.".format(urn),
             )
             return True
