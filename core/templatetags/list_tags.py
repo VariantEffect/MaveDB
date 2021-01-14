4a806d3fcdafa7c6afc2c423dea50fcaaa1a5e87
daniaki <danielce90@gmail.com>
2021-01-14 17:21:52 +1100

    Humanize list

--- core/templatetags/list_tags.py
+++ core/templatetags/list_tags.py
@@ -8,3 +8,18 @@ def is_in(item, container, javascript=False):
     return (
         str(item in container).lower() if javascript else (item in container)
     )
+
+
+@register.filter
+def humanize_list(value):
+    if len(value) == 0:
+        return ""
+    elif len(value) == 1:
+        return value[0]
+
+    s = ", ".join(value[:-1])
+
+    if len(value) > 3:
+        s += ","
+
+    return f"{s} and {value[-1]}"
