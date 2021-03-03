f1ffa82c2b3735203043319d153cd0b37b94e84e
daniaki <danielce90@gmail.com>
2021-03-03 17:42:11 +1100

    fixed check using file name and not home path

--- docker/postgres/docker-entrypoint-initdb.d/restore.sh
+++ docker/postgres/docker-entrypoint-initdb.d/restore.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Running postgres restore script"
 
-if [[ -f "$MAVEDB_DUMP_FILE" ]]; then
+if [[ -f "/home/dumps/${MAVEDB_DUMP_FILE}" ]]; then
   echo "Restoring database dump from file ${MAVEDB_DUMP_FILE}"
   pg_restore -Fc -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" < "/home/dumps/${MAVEDB_DUMP_FILE}"
 fi
